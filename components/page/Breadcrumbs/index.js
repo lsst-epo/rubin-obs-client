408bf70850ae2ec3c2e93ae6a0b26b8ae89cd861
Dana Johnson <dana@castironcoding.com>
2022-02-09 13:02:20 -0800

    [B] Implement accessible breadcrumbs
    Resolves #59

--- components/page/Breadcrumbs/index.js
+++ components/page/Breadcrumbs/index.js
@@ -1,97 +1,22 @@
 import PropTypes from "prop-types";
-import styled from "styled-components";
 import Link from "next/link";
 import internalLinkShape from "@/shapes/link";
-import { containerRegular, respond } from "@/styles/globalStyles";
+import * as Styled from "./styles";
 
 export default function Breadcrumbs({ breadcrumbs, type }) {
   if (!breadcrumbs || breadcrumbs.length < 1) return null;
 
-  return type === "search" ? (
-    <SearchNav>
-      <SearchList>
-        {breadcrumbs.map((breadcrumb) => (
-          <Item key={breadcrumb.id} active={breadcrumb.active}>
-            <Link href={`/${breadcrumb.uri}`}>
-              <a>{breadcrumb.title}</a>
-            </Link>
-          </Item>
-        ))}
-      </SearchList>
-    </SearchNav>
-  ) : (
-    <Nav>
-      <List>
-        {breadcrumbs.map((breadcrumb) => (
-          <Item key={breadcrumb.id} active={breadcrumb.active}>
-            <Link href={`/${breadcrumb.uri}`}>
-              <a>{breadcrumb.title}</a>
-            </Link>
-          </Item>
-        ))}
-      </List>
-    </Nav>
+  return (
+    <Styled.Breadcrumbs breadcrumbs={breadcrumbs} $type={type}>
+      {({ id, uri, title, ...restProps }) => (
+        <Link href={`/${uri}`} passHref>
+          <Styled.Link {...restProps}>{title}</Styled.Link>
+        </Link>
+      )}
+    </Styled.Breadcrumbs>
   );
 }
 
-const SearchNav = styled.nav`
-  font-size: 16px;
-  font-weight: 700;
-`;
-
-const SearchList = styled.ul`
-  display: flex;
-  li {
-    &:first-child {
-      margin-left: 0;
-      a {
-        padding-left: 0;
-      }
-    }
-  }
-`;
-
-const Nav = styled.nav`
-  padding-top: 1.531em;
-  padding-bottom: 1.531em;
-  font-size: 16px;
-  font-weight: 700;
-  background-color: var(--neutral10);
-
-  ${respond(`display: none;`, "450px")}
-`;
-
-const List = styled.ul`
-  ${containerRegular()}
-  display: flex;
-`;
-
-const Item = styled.li`
-  &:first-child {
-    margin-left: -1 * 1em;
-  }
-
-  & + & {
-    position: relative;
-
-    &::after {
-      position: absolute;
-      top: 0;
-      left: 0;
-      display: block;
-      content: "|";
-      transform: translateX(-50%);
-    }
-  }
-
-  a {
-    display: block;
-    padding-right: 1em;
-    padding-left: 1em;
-    text-decoration: none;
-    ${(p) => p.active && `color: var(--turquoise55)`}
-  }
-`;
 Breadcrumbs.displayName = "Global.Breadcrumbs";
 
 Breadcrumbs.propTypes = {
---
+++ components/page/Breadcrumbs/styles.js
@@ -0,0 +1,76 @@
+import styled from "styled-components";
+import CommonJsBreadcrumbs from "@castiron/components-breadcrumbs";
+import { containerRegular, respond } from "@/styles/globalStyles";
+
+const BreadcrumbsWrapper = ({ className, ...restProps }) => {
+  const classes = {
+    ol: `${className}__ol`,
+    li: `${className}__li`,
+  };
+
+  return (
+    <CommonJsBreadcrumbs
+      className={className}
+      classes={classes}
+      {...restProps}
+    />
+  );
+};
+
+export const Breadcrumbs = styled(BreadcrumbsWrapper)`
+  padding-top: 1.531em;
+  padding-bottom: 1.531em;
+  font-size: 16px;
+  font-weight: 700;
+  background-color: var(--neutral10);
+
+  ${respond(`display: none;`, "450px")}
+
+  &__ol {
+    display: flex;
+
+    ${({ $type }) =>
+      $type === "search"
+        ? `
+        li {
+          &:first-child {
+            margin-left: 0;
+            a {
+              padding-left: 0;
+            }
+          }
+        }
+      `
+        : containerRegular()}
+  }
+
+  &__li {
+    &:first-child {
+      margin-left: -1em;
+    }
+
+    & + & {
+      position: relative;
+
+      &::after {
+        position: absolute;
+        top: 0;
+        left: 0;
+        display: block;
+        content: "|";
+        transform: translateX(-50%);
+      }
+    }
+  }
+`;
+
+export const Link = styled.a`
+  display: block;
+  padding-right: 1em;
+  padding-left: 1em;
+  text-decoration: none;
+
+  &[aria-current="page"] {
+    color: var(--turquoise55);
+  }
+`;
