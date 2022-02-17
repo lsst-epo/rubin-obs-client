d571834a48556c87d4846d2aadcc949746fde7ea
jen-castiron <jen@castironcoding.com>
2022-02-17 13:18:24 -0800

    [B] Fix LinkContentBlock url - Resolves #7

--- components/content-blocks/Link/index.js
+++ components/content-blocks/Link/index.js
@@ -4,11 +4,13 @@ import Container from "@/layout/Container";
 import { mixedLinkShape } from "@/shapes/link";
 
 export default function LinkContentBlock({ mixedLink, url, text }) {
+  console.info("LinkContentBlock", mixedLink, url, text);
+
   return (
     <Container>
       <MixedLink
         {...mixedLink}
-        url={url}
+        url={url || mixedLink?.url}
         text={text || mixedLink?.customText || mixedLink?.text}
         className={mixedLink && "c-buttonish c-buttonish--block"}
       />
