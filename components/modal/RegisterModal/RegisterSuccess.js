84e6b16204d07723817246b851fee3c1b7d6b7cf
jen-castiron <jen@castironcoding.com>
2022-03-01 11:54:47 -0800

    [F] Add reset password success message

--- components/modal/RegisterModal/RegisterSuccess.js
+++ components/modal/RegisterModal/RegisterSuccess.js
@@ -3,33 +3,28 @@ import { Button } from "@/components/atomic";
 import * as Styled from "./styles";
 
 export default function RegisterSuccess({ role, onClose }) {
-  return (
+  return role === "teacher" ? (
     <>
-      {role === "teacher" ? (
-        <>
-          <h2>Approval pending</h2>
-          <p>
-            Your account is now created, but approval is pending. You’ll receive
-            an email notification once it is approved and ready to enjoy access
-            to real astronomy data and tools.{" "}
-            <strong>Free of charge forever.</strong>
-          </p>
-          <Styled.FormButtons>
-            <Button onClick={onClose}>Got it!</Button>
-          </Styled.FormButtons>
-        </>
-      ) : (
-        <>
-          <h2>Success!</h2>
-          <p>
-            Your account has been successfully created! Enjoy access to real
-            astronomy data and tools. <strong>Free of charge forever.</strong>
-          </p>
-          <Styled.FormButtons>
-            <Button onClick={onClose}>Got it!</Button>
-          </Styled.FormButtons>
-        </>
-      )}
+      <h2>Approval pending</h2>
+      <p>
+        Your account is now created, but approval is pending. You’ll receive an
+        email notification once it is approved and ready to enjoy access to real
+        astronomy data and tools. <strong>Free of charge forever.</strong>
+      </p>
+      <Styled.FormButtons>
+        <Button onClick={onClose}>Got it!</Button>
+      </Styled.FormButtons>
+    </>
+  ) : (
+    <>
+      <h2>Success!</h2>
+      <p>
+        Your account has been successfully created! Enjoy access to real
+        astronomy data and tools. <strong>Free of charge forever.</strong>
+      </p>
+      <Styled.FormButtons>
+        <Button onClick={onClose}>Got it!</Button>
+      </Styled.FormButtons>
     </>
   );
 }
