8b17e673bb87729b23cf160e89f111789090ca4c
jen-castiron <jen@castironcoding.com>
2022-03-02 12:39:23 -0800

    [F] Add error handling to auth modals

--- components/form/index.js
+++ components/form/index.js
@@ -1,3 +1,4 @@
 export { default as Input } from "./Input";
 export { default as FormField } from "./FormField";
 export { default as FormButtons } from "./FormButtons";
+export { default as Error } from "./Error";
