7a73f31f85641ffa108b70d5aa3ae58db1f6e1c1
jen-castiron <jen@castironcoding.com>
2022-03-02 14:03:36 -0800

    [F] Add all auth translations - reorg auth translations

--- components/modal/RegisterModal/index.js
+++ components/modal/RegisterModal/index.js
@@ -1,15 +1,19 @@
 import React, { useState } from "react";
 import { useRouter } from "next/router";
+import { useTranslation, Trans } from "react-i18next";
 import RegisterForm from "./RegisterForm";
-import RegisterRoleForm from "./RegisterRoleForm";
-import RegisterSuccess from "./RegisterSuccess";
 import useAuthModal from "@/hooks/useAuthModal";
+import { Button } from "@/components/atomic";
 import AuthModal from "../AuthModal";
+import JoinForm from "./JoinForm";
+import * as Styled from "./RegisterForm/styles";
 
 export default function RegisterModal() {
   const { query } = useRouter();
 
   const { openModal, closeModal } = useAuthModal();
+
+  const { t } = useTranslation();
 
   const [formState, setFormState] = useState({
     role: "student",
@@ -40,7 +44,19 @@ export default function RegisterModal() {
       image={formState.success ? undefined : formState.role}
     >
       {formState.success ? (
-        <RegisterSuccess role={query.role} onClose={onClose} />
+        <>
+          <h2>{t("register.success", { context: query.role })}</h2>
+          <p>
+            <Trans
+              i18nKey="register.success_message"
+              values={{ context: query.role }}
+              components={[<strong key="bold"></strong>]}
+            />
+          </p>
+          <Styled.FormButtons>
+            <Button onClick={onClose}>{t("register.confirm_button")}</Button>
+          </Styled.FormButtons>
+        </>
       ) : query.role === "teacher" || query.role === "student" ? (
         <RegisterForm
           role={query.role}
@@ -48,10 +64,10 @@ export default function RegisterModal() {
           onCancel={onCancel}
         />
       ) : (
-        <RegisterRoleForm
-          role={formState.role}
-          onRoleChange={(selectedRole) => setFormState({ role: selectedRole })}
+        <JoinForm
           onEmailSignup={onEmailSignup}
+          onRoleChange={(role) => setFormState({ role: role })}
+          role={formState.role}
         />
       )}
     </AuthModal>
