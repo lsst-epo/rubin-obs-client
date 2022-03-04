ea7be9252f17b4e2f1e41ea68c6e86c96f481a05
jen-castiron <jen@castironcoding.com>
2022-03-04 12:02:27 -0800

    [WIP] Log in with Google

--- components/modal/RegisterModal/JoinForm/index.js
+++ components/modal/RegisterModal/JoinForm/index.js
@@ -4,11 +4,59 @@ import { Button, Link as BaseLink, SSOButton } from "@/components/atomic";
 import * as Styled from "./styles";
 import AuthModal from "../../AuthModal";
 import useAuthModal from "@/hooks/useAuthModal";
+import useAuthentication from "@/hooks/useAuthentication";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function JoinForm({ onEmailSignup, onRoleChange, role }) {
   const { t } = useTranslation();
 
   const { getModalUrl } = useAuthModal();
+
+  const { query } = useRouter();
+
+  const {
+    authenticateWithGoogle,
+    authenticateWithFacebook,
+    getFacebookAuthUrl,
+    getGoogleAuthUrl,
+  } = useAuthentication();
+
+  /** WIP: If the query includes a code, sign in with google */
+  useEffect(() => {
+    if (query.code) {
+      console.info("Fetching token...");
+      const fetchToken = async () => {
+        const data = await authenticateWithGoogle({ code: query.code, role });
+
+        return data;
+      };
+
+      fetchToken()
+        .then((data) => console.info(data))
+        .catch(console.error);
+    }
+  }, [query, authenticateWithGoogle, role]);
+
+  /** WIP: google auth */
+  const onGoogleSignIn = async () => {
+    const googleOauthUrl = getGoogleAuthUrl();
+
+    window.open(googleOauthUrl, "_blank");
+
+    return {};
+  };
+
+  /** WIP: facebook auth */
+  const onFacebookSignIn = async () => {
+    const data = await getFacebookAuthUrl();
+
+    console.info("onFacebookSignIn", data);
+
+    if (data?.facebookOauthUrl) {
+      window.open(data.facebookOauthUrl, "_blank");
+    }
+  };
 
   return (
     <>
@@ -37,10 +85,10 @@ export default function JoinForm({ onEmailSignup, onRoleChange, role }) {
         </AuthModal.Description>
       </div>
       <Styled.SSOButtons>
-        <SSOButton icon="google" type="button">
+        <SSOButton icon="google" type="button" onClick={onGoogleSignIn}>
           {t("join.continue_with_google")}
         </SSOButton>
-        <SSOButton icon="facebook" type="button">
+        <SSOButton icon="facebook" type="button" onClick={onFacebookSignIn}>
           {t("join.continue_with_facebook")}
         </SSOButton>
         <SSOButton icon="email" type="button" onClick={onEmailSignup}>
