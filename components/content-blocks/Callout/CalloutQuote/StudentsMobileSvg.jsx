6c97051ca16b88d8bb102123b8af588b54f75bdf
jen-castiron <jen@castironcoding.com>
2022-03-17 17:33:37 -0700

    [F] Build and style callout-quote - Resolves PROJ-905

---
+++ components/content-blocks/Callout/CalloutQuote/StudentsMobileSvg.jsx
@@ -0,0 +1,207 @@
+export default function StudentsMobileSvg({ image, ...props }) {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={240}
+      height={240.001}
+      {...props}
+    >
+      <defs>
+        <pattern
+          id="pattern"
+          preserveAspectRatio="xMidYMid slice"
+          width="100%"
+          height="100%"
+          viewBox="0 0 8192 8192"
+        >
+          {image && <image href={image.url} height="8192" width="8192" />}
+        </pattern>
+      </defs>
+      <g data-name="Group 559865">
+        <g data-name="Group 463734">
+          <path
+            data-name="Polygon 29"
+            d="m22.147 60.85 127.555-22.484-44.294 121.697Z"
+            fill="#009fa1"
+          />
+          <g data-name="Artwork 3">
+            <g data-name="Group 463727" transform="rotate(21 -50.804 159.364)">
+              <rect
+                data-name="Rectangle 3525"
+                width={70.744}
+                height={98.504}
+                rx={5.685}
+                transform="translate(0 5.786)"
+                fill="#e1c79b"
+              />
+              <path
+                data-name="Rectangle 3526"
+                fill="#fff"
+                stroke="#fff"
+                strokeMiterlimit={10}
+                strokeWidth={0.299}
+                d="M7.57 13.658h55.604v82.759H7.57z"
+              />
+              <path
+                data-name="Path 490700"
+                d="M14.017 27.709a.759.759 0 1 1-.759-.759.759.759 0 0 1 .759.759Z"
+                fill="#dce0e3"
+              />
+              <path
+                data-name="Path 490701"
+                d="M39.449 34.232a.626.626 0 0 1-.461-.185c-.416-.415-.101-1.065.541-1.972A32.493 32.493 0 0 1 41.8 29.31l.345-.378.755.68-.34.378a30.9 30.9 0 0 0-2.226 2.708c.4-.279.957-.709 1.733-1.371l.387-.33.66.773-.387.33a22.282 22.282 0 0 1-2.109 1.632 2.469 2.469 0 0 1-1.169.5Z"
+                fill="#dce0e3"
+              />
+              <path
+                data-name="Path 490702"
+                d="m49.28 25.619-.771-.662.332-.385c.691-.8 1.16-1.41 1.469-1.848a30.992 30.992 0 0 0-2.7 2.224l-.383.339-.681-.755.378-.345a32.471 32.471 0 0 1 2.765-2.271c.907-.642 1.557-.957 1.968-.541s.049 1.086-.338 1.671a23.5 23.5 0 0 1-1.708 2.188Z"
+                fill="#dce0e3"
+              />
+              <g data-name="Group 463725">
+                <path
+                  data-name="Path 490703"
+                  d="M48.166 25.356a3.69 3.69 0 1 1-5.2-.491 3.69 3.69 0 0 1 5.2.491Z"
+                  fill="#dce0e3"
+                />
+              </g>
+              <path
+                data-name="Path 490704"
+                d="m42.34 32.43-.66-.774.387-.33a64.397 64.397 0 0 0 3.558-3.315 63.867 63.867 0 0 0 3.214-3.441l.332-.385.771.663-.331.385a66.697 66.697 0 0 1-3.266 3.5 66.84 66.84 0 0 1-3.619 3.367Z"
+                fill="#dce0e3"
+              />
+              <path
+                data-name="Path 490705"
+                d="M53.871 27.709a1.518 1.518 0 1 1-1.517-1.518 1.517 1.517 0 0 1 1.517 1.518Z"
+                fill="#dce0e3"
+              />
+              <path
+                data-name="Path 490706"
+                d="M27.324 27.709a.936.936 0 1 1-.937-.937.936.936 0 0 1 .937.937Z"
+                fill="#dce0e3"
+              />
+              <path
+                data-name="Path 490707"
+                d="M18.742 27.709a1.483 1.483 0 1 1-1.483-1.483 1.483 1.483 0 0 1 1.483 1.483Z"
+                fill="#dce0e3"
+              />
+              <g data-name="Group 463726">
+                <path
+                  data-name="Path 490708"
+                  d="M24.284 27.712a1.9 1.9 0 1 1-1.894-1.9 1.9 1.9 0 0 1 1.894 1.9Z"
+                  fill="#dce0e3"
+                />
+              </g>
+              <path
+                data-name="Path 490709"
+                d="M40.184 27.709a5.691 5.691 0 1 1-5.691-5.691 5.691 5.691 0 0 1 5.691 5.691Z"
+                fill="#dce0e3"
+              />
+              <path
+                data-name="Path 490710"
+                d="M58.245 27.705a1.3 1.3 0 1 1-1.308-1.3 1.3 1.3 0 0 1 1.308 1.3Z"
+                fill="#dce0e3"
+              />
+              <g data-name="Path 490711" fill="#d5d5d7" strokeMiterlimit={10}>
+                <path d="M56.856 15.512H13.888V5.306h14.651V-.478H42.991V5.307h13.866V15.512ZM35.765 2.326c-.765 0-1.387.622-1.387 1.386a1.388 1.388 0 0 0 2.773 0c0-.764-.622-1.386-1.386-1.386Z" />
+                <path
+                  d="M29.018 0v5.786H14.367v9.247h42.01V5.786H42.511V0H29.018m6.747 5.578a1.865 1.865 0 1 1 0-3.73 1.865 1.865 0 0 1 0 3.73M28.06-.958h15.41v5.786h13.865V15.99H13.41V4.828H28.06V-.958Zm7.705 5.578a.908.908 0 0 0 0-1.815.908.908 0 0 0 0 1.815Z"
+                  fill="#fff"
+                />
+              </g>
+              <path
+                data-name="Rectangle 3527"
+                fill="none"
+                stroke="#9e9fa2"
+                strokeLinecap="round"
+                strokeMiterlimit={10}
+                strokeWidth={2.395}
+                d="M17.016 41.476h6.695v6.695h-6.695z"
+              />
+              <path
+                data-name="Line 8629"
+                fill="none"
+                stroke="#9e9fa2"
+                strokeLinecap="round"
+                strokeMiterlimit={10}
+                strokeWidth={2.395}
+                d="M28.389 44.7h28.019"
+              />
+              <path
+                data-name="Rectangle 3528"
+                fill="none"
+                stroke="#9e9fa2"
+                strokeLinecap="round"
+                strokeMiterlimit={10}
+                strokeWidth={2.395}
+                d="M17.016 52.553h6.695v6.695h-6.695z"
+              />
+              <path
+                data-name="Line 8630"
+                fill="none"
+                stroke="#9e9fa2"
+                strokeLinecap="round"
+                strokeMiterlimit={10}
+                strokeWidth={2.395}
+                d="M28.389 55.776h28.019"
+              />
+              <path
+                data-name="Rectangle 3529"
+                fill="none"
+                stroke="#9e9fa2"
+                strokeLinecap="round"
+                strokeMiterlimit={10}
+                strokeWidth={2.395}
+                d="M17.016 63.63h6.695v6.695h-6.695z"
+              />
+              <path
+                data-name="Line 8631"
+                fill="none"
+                stroke="#9e9fa2"
+                strokeLinecap="round"
+                strokeMiterlimit={10}
+                strokeWidth={2.395}
+                d="M28.389 66.853h28.019"
+              />
+              <path
+                data-name="Rectangle 3530"
+                fill="none"
+                stroke="#9e9fa2"
+                strokeLinecap="round"
+                strokeMiterlimit={10}
+                strokeWidth={2.395}
+                d="M17.016 74.707h6.695v6.695h-6.695z"
+              />
+              <path
+                data-name="Line 8632"
+                fill="none"
+                stroke="#9e9fa2"
+                strokeLinecap="round"
+                strokeMiterlimit={10}
+                strokeWidth={2.395}
+                d="M28.389 77.93h28.019"
+              />
+            </g>
+          </g>
+        </g>
+        <g data-name="Group 463741" transform="translate(15 -664.999)">
+          <circle
+            data-name="Ellipse 4"
+            cx={80}
+            cy={80}
+            r={80}
+            transform="translate(65 745)"
+            fill="#fff"
+          />
+          <circle
+            data-name="Artwork 4"
+            cx={56}
+            cy={56}
+            r={56}
+            transform="translate(89 769)"
+            fill="url(#pattern)"
+          />
+        </g>
+      </g>
+    </svg>
+  );
+}
---
+++ components/content-blocks/Callout/CalloutQuote/StudentsSvg.jsx
@@ -0,0 +1,200 @@
+export default function StudentsSvg({ image, ...props }) {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width={249} height={449} {...props}>
+      <defs>
+        <pattern
+          id="a"
+          preserveAspectRatio="xMidYMid slice"
+          width="100%"
+          height="100%"
+          viewBox="0 0 8192 8192"
+        >
+          {image && <image width="8192" height="8192" href={image.url} />}
+        </pattern>
+      </defs>
+      <g data-name="Group 559864">
+        <path
+          data-name="Polygon 31"
+          d="m34.206 93.98 197.007-34.725-68.412 187.96Z"
+          fill="#009fa1"
+        />
+        <g data-name="Group 463804" transform="translate(-450 -1392)">
+          <circle
+            data-name="Ellipse 4"
+            cx={124.5}
+            cy={124.5}
+            r={124.5}
+            transform="translate(450 1592)"
+            fill="#fff"
+          />
+          <circle
+            data-name="Artwork 4"
+            cx={61.5}
+            cy={61.5}
+            r={61.5}
+            transform="translate(513 1655)"
+            fill="url(#a)"
+          />
+        </g>
+        <g data-name="Artwork 3">
+          <g data-name="Group 463808" transform="rotate(21 -78.467 246.136)">
+            <rect
+              data-name="Rectangle 3644"
+              width={109.263}
+              height={152.138}
+              rx={8.781}
+              transform="translate(0 8.936)"
+              fill="#e1c79b"
+            />
+            <path
+              data-name="Rectangle 3645"
+              fill="#fff"
+              stroke="#fff"
+              strokeMiterlimit={10}
+              strokeWidth={0.462}
+              d="M11.692 21.095h85.879v127.82H11.692z"
+            />
+            <path
+              data-name="Path 490916"
+              d="M21.649 42.796a1.172 1.172 0 1 1-1.172-1.172 1.172 1.172 0 0 1 1.172 1.172Z"
+              fill="#dce0e3"
+            />
+            <path
+              data-name="Path 490917"
+              d="M60.926 52.865a.967.967 0 0 1-.71-.285c-.642-.642-.156-1.639.836-3.041a50.185 50.185 0 0 1 3.515-4.271l.526-.583 1.166 1.051-.525.583a47.728 47.728 0 0 0-3.439 4.183c.615-.431 1.479-1.1 2.676-2.118l.6-.51 1.017 1.194-.6.51a34.414 34.414 0 0 1-3.253 2.521 3.813 3.813 0 0 1-1.809.766Z"
+              fill="#dce0e3"
+            />
+            <path
+              data-name="Path 490918"
+              d="m76.111 39.568-1.19-1.024.512-.6c1.066-1.236 1.791-2.172 2.268-2.849a47.867 47.867 0 0 0-4.178 3.434l-.583.525-1.051-1.165.583-.525a50.15 50.15 0 0 1 4.27-3.515c1.4-.992 2.4-1.478 3.04-.836.657.657.076 1.677-.521 2.581a36.3 36.3 0 0 1-2.638 3.379Z"
+              fill="#dce0e3"
+            />
+            <g data-name="Group 463806">
+              <path
+                data-name="Path 490919"
+                d="M74.392 39.163a5.7 5.7 0 1 1-8.025-.759 5.7 5.7 0 0 1 8.025.759Z"
+                fill="#dce0e3"
+              />
+            </g>
+            <path
+              data-name="Path 490920"
+              d="m65.394 50.088-1.019-1.195.6-.51c1.677-1.431 3.577-3.2 5.5-5.12 1.838-1.844 3.555-3.682 4.957-5.309l.512-.6 1.19 1.024-.512.6a101.103 101.103 0 0 1-5.047 5.396c-1.947 1.947-3.879 3.747-5.587 5.2Z"
+              fill="#dce0e3"
+            />
+            <path
+              data-name="Path 490921"
+              d="M83.203 42.796a2.344 2.344 0 1 1-2.344-2.344 2.344 2.344 0 0 1 2.344 2.344Z"
+              fill="#dce0e3"
+            />
+            <path
+              data-name="Path 490922"
+              d="M42.201 42.796a1.446 1.446 0 1 1-1.446-1.446 1.446 1.446 0 0 1 1.446 1.446Z"
+              fill="#dce0e3"
+            />
+            <path
+              data-name="Path 490923"
+              d="M28.947 42.796a2.29 2.29 0 1 1-2.29-2.291 2.291 2.291 0 0 1 2.29 2.291Z"
+              fill="#dce0e3"
+            />
+            <g data-name="Group 463807">
+              <path
+                data-name="Path 490924"
+                d="M37.506 42.796a2.93 2.93 0 1 1-2.929-2.93 2.93 2.93 0 0 1 2.929 2.93Z"
+                fill="#dce0e3"
+              />
+            </g>
+            <path
+              data-name="Path 490925"
+              d="M62.063 42.796a8.789 8.789 0 1 1-8.79-8.789 8.789 8.789 0 0 1 8.79 8.789Z"
+              fill="#dce0e3"
+            />
+            <path
+              data-name="Path 490926"
+              d="M89.957 42.796a2.014 2.014 0 1 1-2.013-2.014 2.014 2.014 0 0 1 2.013 2.014Z"
+              fill="#dce0e3"
+            />
+            <g data-name="Path 490927" fill="#d5d5d7" strokeMiterlimit={10}>
+              <path d="M87.813 23.958H21.449V8.196H44.078V-.74H66.398v8.936h21.415v15.762ZM55.237 3.593c-1.18 0-2.14.96-2.14 2.14 0 1.181.96 2.142 2.14 2.142 1.181 0 2.142-.96 2.142-2.141 0-1.18-.96-2.141-2.142-2.141Z" />
+              <path
+                d="M44.818 0v8.936H22.189v14.282h64.884V8.936H65.657V0h-20.84m10.42 8.615a2.88 2.88 0 1 1 0-5.762 2.88 2.88 0 0 1 0 5.762M43.337-1.48h23.8v8.936h21.416v17.242H20.709V7.456h22.629V-1.48Zm11.9 8.615c.773 0 1.402-.629 1.402-1.401a1.403 1.403 0 0 0-2.803 0c0 .772.629 1.401 1.401 1.401Z"
+                fill="#fff"
+              />
+            </g>
+            <path
+              data-name="Rectangle 3646"
+              fill="none"
+              stroke="#9e9fa2"
+              strokeLinecap="round"
+              strokeMiterlimit={10}
+              strokeWidth={3.699}
+              d="M26.281 64.059h10.34v10.34h-10.34z"
+            />
+            <path
+              data-name="Line 8764"
+              fill="none"
+              stroke="#9e9fa2"
+              strokeLinecap="round"
+              strokeMiterlimit={10}
+              strokeWidth={3.699}
+              d="M43.847 69.038h43.274"
+            />
+            <path
+              data-name="Rectangle 3647"
+              fill="none"
+              stroke="#9e9fa2"
+              strokeLinecap="round"
+              strokeMiterlimit={10}
+              strokeWidth={3.699}
+              d="M26.281 81.168h10.34v10.34h-10.34z"
+            />
+            <path
+              data-name="Line 8765"
+              fill="none"
+              stroke="#9e9fa2"
+              strokeLinecap="round"
+              strokeMiterlimit={10}
+              strokeWidth={3.699}
+              d="M43.847 86.146h43.274"
+            />
+            <path
+              data-name="Rectangle 3648"
+              fill="none"
+              stroke="#9e9fa2"
+              strokeLinecap="round"
+              strokeMiterlimit={10}
+              strokeWidth={3.699}
+              d="M26.281 98.276h10.34v10.34h-10.34z"
+            />
+            <path
+              data-name="Line 8766"
+              fill="none"
+              stroke="#9e9fa2"
+              strokeLinecap="round"
+              strokeMiterlimit={10}
+              strokeWidth={3.699}
+              d="M43.847 103.254h43.274"
+            />
+            <path
+              data-name="Rectangle 3649"
+              fill="none"
+              stroke="#9e9fa2"
+              strokeLinecap="round"
+              strokeMiterlimit={10}
+              strokeWidth={3.699}
+              d="M26.281 115.384h10.34v10.34h-10.34z"
+            />
+            <path
+              data-name="Line 8767"
+              fill="none"
+              stroke="#9e9fa2"
+              strokeLinecap="round"
+              strokeMiterlimit={10}
+              strokeWidth={3.699}
+              d="M43.847 120.362h43.274"
+            />
+          </g>
+        </g>
+      </g>
+    </svg>
+  );
+}
