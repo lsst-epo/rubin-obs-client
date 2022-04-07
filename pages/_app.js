dc84198b03d2b5aaf9ef36067ee2ace16387ad0c
Blake Mason <bmason@lsst.org>
2022-04-07 15:58:17 -0700

    [C] Add pageproofer snippet
    Pageproofer will allow internal stakeholders to provide feedback in a "sticky note" fashion

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import PropTypes from "prop-types";
 import "@/lib/i18n";
 import "focus-visible";
 import { UIDReset } from "react-uid";
+import Script from "next/script";
 import { AuthenticationContextProvider } from "@/contexts/Authentication";
 import useAuthentication from "@/hooks/useAuthentication";
 import GlobalStyles from "@/styles/globalStyles";
@@ -19,6 +20,11 @@ function Client({ Component, pageProps }) {
   return (
     <UIDReset>
       <AuthenticationContextProvider data={authData}>
+        <Script
+          id="page-proofer-snippet"
+          src="https://app.pageproofer.com/embed/77e83361-ba26-51f2-978b-850fb00a263e"
+          strategy="lazyOnload"
+        />
         <GlobalStyles />
         <Component {...pageProps} />
       </AuthenticationContextProvider>
