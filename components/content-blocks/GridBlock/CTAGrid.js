b3a36a321eb33136f2a036bec4c0f28854331a38
jen-castiron <jen@castironcoding.com>
2022-04-07 19:43:35 -0700

    [F] Build Investigation grid

--- components/content-blocks/GridBlock/CTAGrid.js
+++ components/content-blocks/GridBlock/CTAGrid.js
@@ -1,23 +1,34 @@
 import PropTypes from "prop-types";
 import Grid from "@/layout/Grid";
 import Tile from "@/atomic/Tile";
+import InvestigationTile from "@/components/atomic/Tile/patterns/InvestigationTile";
+
+const tileMap = {
+  investigation: InvestigationTile,
+};
 
 const CTAGrid = ({ items, limit }) => {
   const cols = limit === 4 ? 4 : 3;
+
+  const renderTile = ({ id, contentImage, mixedLink }) => {
+    const TileBlock = tileMap[mixedLink.element.typeHandle] || Tile;
+
+    return (
+      <TileBlock
+        key={id}
+        title={mixedLink?.customText || mixedLink?.text}
+        image={contentImage?.[0]}
+        link={mixedLink}
+        type="cta"
+      />
+    );
+  };
 
   return (
     <>
       {items.length > 0 && (
         <Grid columns={cols} tablet={3}>
-          {items.map(({ id, contentImage, mixedLink }) => (
-            <Tile
-              key={id}
-              title={mixedLink?.customText || mixedLink?.text}
-              image={contentImage?.[0]}
-              link={mixedLink}
-              type="cta"
-            />
-          ))}
+          {items.map(renderTile)}
         </Grid>
       )}
     </>
