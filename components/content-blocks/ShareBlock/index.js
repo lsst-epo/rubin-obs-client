f85f88a03cd462c8f75b8688df0e375813c2f6c7
Blake Mason <bmason@lsst.org>
2022-04-11 15:50:16 -0700

    [C] Update env vars in workflows and hardcode where needed at runtime

--- components/content-blocks/ShareBlock/index.js
+++ components/content-blocks/ShareBlock/index.js
@@ -1,7 +1,9 @@
 import { InlineShare } from "@/components/atomic/Share";
 import Container from "@/layout/Container";
 
-const SHARETHIS_PROPERTY = process.env.NEXT_PUBLIC_SHARETHIS_PROPERTY;
+// const SHARETHIS_PROPERTY = process.env.NEXT_PUBLIC_SHARETHIS_PROPERTY;
+// Should be replaced with an env var
+const SHARETHIS_PROPERTY = "5f57f7661db73a00129d7d03";
 
 export default function ShareContentBlock() {
   if (!SHARETHIS_PROPERTY) return null;
