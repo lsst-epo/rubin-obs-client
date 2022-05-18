77d8be57ce6de5ce2b943f7ee7ac0b1c60c54710
Ella Tanttu <ellajtanttu@gmail.com>
2022-05-18 14:56:42 -0700

    WIP: Update grid l tiles as list items

--- components/content-blocks/GridBlock/CTAGrid.js
+++ components/content-blocks/GridBlock/CTAGrid.js
@@ -1,32 +1,35 @@
-import PropTypes from "prop-types";
-import Grid from "@/layout/Grid";
-import Tile from "@/atomic/Tile";
+import PropTypes from 'prop-types'
+import Grid from '@/layout/Grid'
+import Tile from '@/atomic/Tile'
 
 const CTAGrid = ({ items, limit }) => {
-  const cols = limit === 4 ? 4 : 3;
+  const cols = limit === 4 ? 4 : 3
 
   return (
     <>
       {items.length > 0 && (
         <Grid columns={cols} tablet={3}>
-          {items.map(({ id, contentImage, mixedLink }) => (
-            <Tile
-              key={id}
-              title={mixedLink?.customText || mixedLink?.text}
-              image={contentImage?.[0]}
-              link={mixedLink}
-              type="cta"
-            />
-          ))}
+          <ul>
+            {items.map(({ id, contentImage, mixedLink }) => (
+              <li key={id}>
+                <Tile
+                  title={mixedLink?.customText || mixedLink?.text}
+                  image={contentImage?.[0]}
+                  link={mixedLink}
+                  type="cta"
+                />
+              </li>
+            ))}
+          </ul>
         </Grid>
       )}
     </>
-  );
-};
+  )
+}
 
 CTAGrid.propTypes = {
   items: PropTypes.array,
   limit: PropTypes.number,
-};
+}
 
-export default CTAGrid;
+export default CTAGrid
