c99955375dd298b65021ddde71b90d04c461c51f
Ella Tanttu <ellajtanttu@gmail.com>
2022-05-19 10:45:42 -0700

    WIP: Update gridblocks to include ul/li tags

--- components/content-blocks/GridBlock/CTAGrid.js
+++ components/content-blocks/GridBlock/CTAGrid.js
@@ -1,35 +1,32 @@
-import PropTypes from 'prop-types'
-import Grid from '@/layout/Grid'
-import Tile from '@/atomic/Tile'
+import PropTypes from "prop-types";
+import Grid from "@/layout/Grid";
+import Tile from "@/atomic/Tile";
 
 const CTAGrid = ({ items, limit }) => {
-  const cols = limit === 4 ? 4 : 3
+  const cols = limit === 4 ? 4 : 3;
 
   return (
     <>
       {items.length > 0 && (
         <Grid columns={cols} tablet={3}>
-          <ul>
-            {items.map(({ id, contentImage, mixedLink }) => (
-              <li key={id}>
-                <Tile
-                  title={mixedLink?.customText || mixedLink?.text}
-                  image={contentImage?.[0]}
-                  link={mixedLink}
-                  type="cta"
-                />
-              </li>
-            ))}
-          </ul>
+          {items.map(({ id, contentImage, mixedLink }) => (
+            <Tile
+              key={id}
+              title={mixedLink?.customText || mixedLink?.text}
+              image={contentImage?.[0]}
+              link={mixedLink}
+              type="cta"
+            />
+          ))}
         </Grid>
       )}
     </>
-  )
-}
+  );
+};
 
 CTAGrid.propTypes = {
   items: PropTypes.array,
   limit: PropTypes.number,
-}
+};
 
-export default CTAGrid
+export default CTAGrid;
