b98eebb637bdb528347a5b0ad60093fd509a5f24
Alexandra Goff <agoff@lsst.org>
2022-06-07 14:16:44 -0700

    [C] fix conditional hook use and dup import

--- components/atomic/Tile/patterns/InvestigationTile/index.js
+++ components/atomic/Tile/patterns/InvestigationTile/index.js
@@ -10,26 +10,33 @@ const InvestigationTile = ({ investigation, useExternalLink }) => {
 
   const { t } = useTranslation();
 
-  if (!investigation) return null;
-
-  const { image, isActive, title, externalUrl, landingPage } = investigation;
+  const { image, isActive, title, externalUrl, landingPage } =
+    investigation || {};
 
   const finalImage = useDamAssetAsImage(image[0]);
 
   const url = useExternalLink ? externalUrl : landingPage?.[0]?.uri;
 
   return (
-    <Styled.MixedLink aria-labelledby={uid} aria-disabled={!isActive} url={url}>
-      <Styled.Image>{finalImage && <Image image={finalImage} />}</Styled.Image>
-      {title && (
-        <Styled.Title id={uid}>
-          <span>
-            {title}
-            {!isActive && <div>{t("investigation.coming_soon")}</div>}
-          </span>
-        </Styled.Title>
-      )}
-    </Styled.MixedLink>
+    investigation && (
+      <Styled.MixedLink
+        aria-labelledby={uid}
+        aria-disabled={!isActive}
+        url={url}
+      >
+        <Styled.Image>
+          {finalImage && <Image image={finalImage} />}
+        </Styled.Image>
+        {title && (
+          <Styled.Title id={uid}>
+            <span>
+              {title}
+              {!isActive && <div>{t("investigation.coming_soon")}</div>}
+            </span>
+          </Styled.Title>
+        )}
+      </Styled.MixedLink>
+    )
   );
 };
 
