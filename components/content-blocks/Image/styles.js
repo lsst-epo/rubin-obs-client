d5640df25a31083da2684844c27cafb13bf14e1a
Dana Johnson <dana@castironcoding.com>
2022-06-22 23:15:15 -0700

    [F] Wire up Image block float direction
    Resolves #13

---
+++ components/content-blocks/Image/styles.js
@@ -0,0 +1,24 @@
+import styled from "styled-components";
+import { containerNarrow, respond, tokens } from "@/styles/globalStyles";
+
+export const Container = styled.section`
+  ${({ $floatDirection }) =>
+    $floatDirection === "right" || $floatDirection === "left"
+      ? respond(
+          `
+        float: ${$floatDirection};
+        inline-size: 50%;
+
+        > figure {
+          ${containerNarrow()}
+          max-inline-size: calc(var(--max-width) * 0.5);
+          margin-inline-${$floatDirection === "right" ? "start" : "end"}: 0;
+          padding-inline: ${tokens.PADDING_MEDIUM};
+          padding-block-start: ${tokens.PADDING_MEDIUM};
+        }
+        `,
+          tokens.BREAK_TABLET_MIN,
+          "min"
+        )
+      : ``}
+`;
