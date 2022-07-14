645e4edca618a60d80942892420d91a8501f85ea
Blake Mason <bmason@lsst.org>
2022-07-14 10:25:42 -0700

    [C] Remove CIC eslint dep and add now missing eslint deps to repo

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,20 +2,93 @@ require("@rushstack/eslint-patch/modern-module-resolution");
 
 module.exports = {
   root: true,
+  parser: "@babel/eslint-parser",
+  parserOptions: {
+    sourceType: "module",
+    requireConfigFile: false,
+    babelOptions: {
+      plugins: [
+        "@babel/plugin-transform-react-jsx",
+        "@babel/plugin-proposal-class-properties",
+      ],
+    },
+  },
   env: {
     browser: true,
   },
+  settings: {
+    react: {
+      version: "detect",
+    },
+  },
   rules: {
+    "array-callback-return": 0,
+    "arrow-body-style": 0,
+    "global-require": 0,
+    "consistent-return": 0,
+    "prefer-template": 0,
+    "no-useless-constructor": 0,
+    "jsx-a11y/img-has-alt": 0,
+    "jsx-a11y/no-onchange": 0,
+    "jsx-a11y/media-has-caption": 1,
     // let next/link package handle anchor attributes
     "jsx-a11y/anchor-is-valid": 0,
     // next/link handles the href, so anchors without href are still interactive
     "jsx-a11y/no-noninteractive-tabindex": ["error", { tags: ["a"] }],
     // throwing false negatives on components using Atomics.Image
     "jsx-a11y/alt-text": 0,
-    // storybook stories are exported this way
-    "import/no-anonymous-default-export": 0,
-    // would be good to use next/image but we aren't yet
-    "@next/next/no-img-element": 0,
+    quotes: 0,
+    // This rule doesn't work well with jsconfig
+    "import/no-unresolved": 0,
+    "jsx-quotes": 2,
+    "block-scoped-var": 0,
+    "comma-dangle": 0,
+    "comma-spacing": 2,
+    "generator-star-spacing": 0,
+    indent: 0,
+    "no-alert": 2,
+    "no-console": ["error", { allow: ["warn", "error", "info"] }],
+    "no-debugger": 2,
+    semi: 0,
+    "no-empty-pattern": 1,
+    "no-extra-semi": 0,
+    "no-unused-semi": 0,
+    "no-unused-vars": [
+      0,
+      {
+        args: "after-used",
+        varsIgnorePattern: "PropTypes",
+        argsIgnorePattern: "[iI]gnored",
+      },
+    ],
+    "no-trailing-spaces": 2,
+    "no-multiple-empty-lines": 2,
+    "no-underscore-dangle": 0,
+    "max-len": [
+      "error",
+      {
+        ignoreComments: true,
+        ignoreStrings: true,
+        ignoreTemplateLiterals: true,
+      },
+    ],
+    "padded-blocks": 0,
+    "prefer-const": 2,
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn",
+    "react/react-in-jsx-scope": 0,
+    "space-before-function-paren": 0,
+    "spaced-comment": 2,
+    "unused-imports/no-unused-imports": 2,
   },
-  extends: ["@castiron", "next"],
+  plugins: ["jsx-a11y", "react", "react-hooks", "unused-imports"],
+  extends: [
+    "standard",
+    "next",
+    "prettier",
+    "plugin:jsx-a11y/recommended",
+    "plugin:react/recommended",
+    "plugin:prettier/recommended",
+    "plugin:import/errors",
+  ],
 };
