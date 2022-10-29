fe8767acc948ad373e6352566e88517e9bb9e966
Blake Mason <bmason@lsst.org>
2022-10-28 17:04:25 -0700

    [F] Hardcoded data2dome PoC json feed

--- pages/api/feeds/[entryType].js
+++ pages/api/feeds/[entryType].js
@@ -5,6 +5,7 @@ import {
   generateFeed,
   writeFeedsToDisk,
 } from "@/api/feeds";
+import { data2domefeed, writeData2domeFeedToDisk } from "@/api/data2domeFeeds";
 
 async function handler(req, res) {
   const { entryType } = req.query;
@@ -13,6 +14,25 @@ async function handler(req, res) {
     return res.status(501).json({
       error: `Feed generation for entry type "${entryType}" is not currently supported. Try one of the following: ${formattedTypesList}.`,
     });
+
+  if (entryType === "data2dome") {
+    try {
+      const publicPath = "feeds/data2dome/";
+      const writePath = `./public/${publicPath}`;
+      writeData2domeFeedToDisk({ path: writePath, feed: data2domefeed }).then(
+        () => {
+          res.status(200).json({
+            message: `Feeds for entry type "${entryType}" have been successfully written to ${writePath}.`,
+          });
+        }
+      );
+    } catch (error) {
+      console.error(error);
+      return res.status(500).json({
+        error: `Failed to generate feeds for entry type "${entryType}".`,
+      });
+    }
+  }
 
   try {
     const entryData = await getEntries([entryType]);
