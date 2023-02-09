c3fa4f942a0ca02e8a39057703eec96373be0e94
Blake Mason <bmason@lsst.org>
2023-02-09 14:01:10 -0800

    [F] Facebook OAuth redirects back to start

---
+++ middleware.js
@@ -0,0 +1,22 @@
+import { NextResponse } from "next/server";
+const NEXT_PUBLIC_BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
+
+// This function can be marked `async` if using `await` inside
+export function middleware(request) {
+  const {
+    nextUrl: { search },
+  } = request;
+  const urlSearchParams = new URLSearchParams(search);
+  const params = Object.fromEntries(urlSearchParams.entries());
+  const { state: redirectPath, facebook, code } = params;
+  if (redirectPath, facebook, code) {
+    const redirectUrl = request.nextUrl.clone();
+    redirectUrl.pathname = redirectPath;
+    return NextResponse.redirect(redirectUrl);
+  }
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: "/sso-redirect/",
+};
