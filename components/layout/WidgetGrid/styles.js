ca400c1e4f535d43ab3a4b4ad7f52c73447b5e10
Alexandra Goff <agoff@lsst.org>
2023-04-26 14:31:48 -0700

    [F] WidgetGrid
    For JIRA: "EPO-7926 #IN-REVIEW #comment add WidgetGrid"

---
+++ components/layout/WidgetGrid/styles.js
@@ -0,0 +1,21 @@
+import { token } from "@/styles/globalStyles";
+import styled from "styled-components";
+
+export const WidgetGrid = styled.div`
+  --widget-row-gap: var(--PADDING_SMALL, 20px);
+  --widget-columns: 2;
+
+  display: grid;
+  grid-auto-flow: row dense;
+  grid-auto-rows: 1fr;
+  grid-template-columns: repeat(var(--widget-columns), 1fr);
+  grid-gap: var(--widget-row-gap);
+
+  @media screen and (min-width: ${token("BREAK_TABLET_MIN")}) {
+    --widget-columns: 3;
+  }
+
+  @media screen and (min-width: ${token("BREAK_DESKTOP_SMALL")}) {
+    --widget-columns: 6;
+  }
+`;
