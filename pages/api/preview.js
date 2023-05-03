c69f621569dd43f2ca300e3fda8def8bebaca173
Blake Mason <bmason@lsst.org>
2023-05-03 09:30:07 -0700

    [B] ES and EN previews both work with dedicated previewUri

--- pages/api/preview.js
+++ pages/api/preview.js
@@ -2,10 +2,11 @@ import { isCraftPreview } from "@/helpers";
 import { getEntryDataByUid } from "@/api/entry";
 
 const preview = async (req, res) => {
+  const PREVIEW_SLUG = process.env.NEXT_PREVIEW_SLUG;
   const { query } = req;
   const isPreview = isCraftPreview(query);
+  // N.B. previewToken isn't consistently available
   const previewToken = query.token || undefined;
-
   if (!query.entryUid)
     return res
       .status(401)
@@ -26,25 +27,23 @@ const preview = async (req, res) => {
 
   // const { pathname } = new URL(entry.url.replace("/es/es", "/es"));
   const { uri } = entry;
-  const previewUri = `/preview-in-craft-cms/${uri}`;
+  // N.B. Because previewToken presence is unreliable, we're always redirecting to previewUri so real pages are never inadvertently updated.
+  // If a previewToken becomes reliable, inclusion of previewToken in the preview data should be enough (and could use real page uri as redirect)
+  const previewUri = `/${PREVIEW_SLUG}`;
 
-  if (previewToken) {
-    // Enable Preview Mode by setting the cookies
-    res.setPreviewData(
-      {
-        previewToken,
-        uri,
-      },
-      {
-        maxAge: 120,
-        path: previewUri,
-      }
-    );
-    // Redirect to the path from the fetched url
-    res.redirect(previewUri);
-  } else {
-    res.redirect(uri);
-  }
+  // Enable Preview Mode by setting the cookies
+  res.setPreviewData(
+    {
+      previewToken,
+      uriSegments: uri.split("/"),
+    },
+    {
+      maxAge: 120,
+      path: previewUri,
+    }
+  );
+  // Redirect to the path from the fetched url
+  res.redirect(previewUri);
 };
 
 export default preview;
