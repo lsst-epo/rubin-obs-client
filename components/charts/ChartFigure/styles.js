a000535c0d1c9c0d0d8f543e0a7b733d4b21de4d
Alexandra Goff <agoff@lsst.org>
2023-05-08 10:54:50 -0700

    [C] scrollbar background

--- components/charts/ChartFigure/styles.js
+++ components/charts/ChartFigure/styles.js
@@ -17,15 +17,19 @@ export const Content = styled.div`
   overflow-x: auto;
   position: relative;
 
-  scrollbar-color: var(--white, #fff) transparent;
+  scrollbar-color: var(--white, #fff) rgba(255, 255, 255, 0.2);
+  scrollbar-width: thin;
 
   &::-webkit-scrollbar {
     height: 8px;
-    background-color: transparent; /* or add it to the track */
+    background-clip: padding-box;
+    border-top: 3px solid transparent;
+    border-bottom: 3px solid transparent;
+    background-color: rgba(255, 255, 255, 0.5); /* or add it to the track */
   }
 
   &::-webkit-scrollbar-thumb {
     border-radius: 4px;
-    background: var(--white, #fff);
+    background-color: var(--white, #fff);
   }
 `;
