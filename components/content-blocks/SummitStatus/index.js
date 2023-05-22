5dffa09ffbbc4e151857b21f97ca8dd9b5294d20
Alexandra Goff <agoff@lsst.org>
2023-05-22 11:15:43 -0700

    [C] minimal summit status setup

--- components/content-blocks/SummitStatus/index.js
+++ components/content-blocks/SummitStatus/index.js
@@ -1,14 +1,23 @@
 import PropTypes from "prop-types";
+import { Container } from "@rubin-epo/epo-react-lib";
+import WidgetGrid from "@/components/layout/WidgetGrid";
+import WidgetPreview from "@/components/layout/WidgetPreview";
 
-const SummitStatus = ({ summitStatusLayout }) => (
-  <section>
-    <h1>Summit Status!</h1>
-    {summitStatusLayout}
-  </section>
+const SummitStatus = ({ summitStatusLayout, widgetPreviews = [] }) => (
+  <Container bgColor="neutral95" width="wide" paddingSize="small">
+    <WidgetGrid>
+      {widgetPreviews.map(({}, i) => (
+        <WidgetPreview key={i}></WidgetPreview>
+      ))}
+    </WidgetGrid>
+  </Container>
 );
 
 SummitStatus.propTypes = {
   summitStatusLayout: PropTypes.oneOf(["compact", "full"]),
+  widgetPreviews: PropTypes.arrayOf({}),
 };
+
+SummitStatus.displayName = "ContentBlock.SummitStatus";
 
 export default SummitStatus;
