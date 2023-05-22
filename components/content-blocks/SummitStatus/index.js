557955ad0dec00fcadb3d87bccf7d79c0aea8d15
Alexandra Goff <agoff@lsst.org>
2023-05-22 10:27:50 -0700

    [C] add proptypes

--- components/content-blocks/SummitStatus/index.js
+++ components/content-blocks/SummitStatus/index.js
@@ -1,8 +1,14 @@
+import PropTypes from "prop-types";
+
 const SummitStatus = ({ summitStatusLayout }) => (
   <section>
     <h1>Summit Status!</h1>
     {summitStatusLayout}
   </section>
 );
+
+SummitStatus.propTypes = {
+  summitStatusLayout: PropTypes.oneOf(["compact", "full"]),
+};
 
 export default SummitStatus;
