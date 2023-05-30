e93cefb75aac5bc0820f8cff1207e2865797b91c
Eric Rosas <ericdrosas@gmail.com>
2023-05-30 14:01:32 -0700

    [F] Add cypress + example e2e test
     - Includes github action for runnig test suite on new dev branch PRs

---
+++ cypress/e2e/homepage.cy.js
@@ -0,0 +1,19 @@
+describe("Search from Homepage", () => {
+  beforeEach(() => {
+    // Cypress starts out with a blank slate for each test
+    // so we must tell it to visit our website with the `cy.visit()` command.
+    // Since we want to visit the same URL at the start of all our tests,
+    // we include it in our beforeEach function so that it runs before each test
+    cy.visit("/");
+  });
+
+  it("Searching yields results", () => {
+    cy.get(".c-search-bar__toggle").click();
+    cy.get("#headerSearchBar").should("be.visible").type("rubin{enter}");
+    cy.intercept("http://localhost:3000/search?search=rubin").as("search");
+    cy.on("url:changed", (newUrl) => {
+      expect(newUrl).to.contain("?search=rubin")
+    })
+    cy.get('ul li').its('length').should('be.gte', 1)
+  });
+});
--- e2e/int-tests/404.js
+++
@@ -1,9 +0,0 @@
-import { Selector } from "testcafe";
-
-fixture`404`.page("./totally-bogus-page-url");
-
-test("404 Page Loads if route does not exist", async (t) => {
-  const errorHeading = await Selector("h1").withText("Page not found");
-
-  await t.expect(errorHeading.exists).ok();
-});
