cef96d527fb78b7322a2b023e0b05cae211ceca8
Alexandra Goff <agoff@lsst.org>
2023-05-31 15:22:42 -0700

    [C] percentage formatting

--- components/widgets/PrecipitationCurrent/styles.js
+++ components/widgets/PrecipitationCurrent/styles.js
@@ -9,6 +9,9 @@ export const WidgetUnit = styled.span`
   line-height: 0.75;
 `;
 export const WidgetValue = styled.span`
+  display: inline-flex;
+  align-items: center;
   font-size: 275%;
+  font-variant-numeric: tabular-nums;
   line-height: 0.75;
 `;
