18a2059317ceaa72a864d822ebded927fbfdbcdd
Alexandra Goff <agoff@lsst.org>
2023-06-23 11:13:03 -0700

    [F] lunar phase setup

--- components/dynamic/SummitData/Astroweather/index.js
+++ components/dynamic/SummitData/Astroweather/index.js
@@ -1,19 +1,23 @@
 import { useState } from "react";
 import { useTranslation } from "react-i18next";
+import SunCalc from "suncalc";
 import WidgetPreview from "@/components/layout/WidgetPreview";
 import SummitStatusModal from "@/components/modal/SummitStatusModal";
+import MoonPhase from "@/components/widgets/CurrentData/patterns/MoonPhase";
 
 const Astroweather = () => {
   const { t } = useTranslation();
   const [isModalOpen, setModalOpen] = useState(false);
 
+  const { phase } = SunCalc.getMoonIllumination(new Date());
   return (
     <WidgetPreview
-      {...previewProps}
+      title="Astroweather"
       openModalCallback={() => {
         setModalOpen(true);
       }}
     >
+      <MoonPhase phase={phase} />
       <SummitStatusModal
         open={isModalOpen}
         onClose={() => setModalOpen(false)}
