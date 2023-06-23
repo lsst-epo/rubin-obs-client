9134ed11b4e43cb3cc9c5d84b34c746591fe6441
Alexandra Goff <agoff@lsst.org>
2023-06-23 12:13:23 -0700

    [C] condense props and labels

--- components/widgets/CurrentData/patterns/MoonPhase/index.js
+++ components/widgets/CurrentData/patterns/MoonPhase/index.js
@@ -1,43 +1,23 @@
 import PropTypes from "prop-types";
-import { useTranslation } from "react-i18next";
+import { Trans } from "react-i18next";
 import WidgetBackground from "@/components/atomic/WidgetBackground";
-import * as Styled from "../../styles";
 import Moon from "@/components/svg/unique/moon";
+import phases from "@/components/svg/unique/moon/parts";
+import * as Styled from "../../styles";
 
 const MoonPhase = ({ phase = 0 }) => {
-  const { t } = useTranslation();
-
-  const labels = [
-    t("summit_dashboard.astro.moon.new"),
-    t("summit_dashboard.astro.moon.crescent", { context: "waxing" }),
-    t("summit_dashboard.astro.moon.crescent", { context: "waxing" }),
-    t("summit_dashboard.astro.moon.crescent", { context: "waxing" }),
-    t("summit_dashboard.astro.moon.crescent", { context: "waxing" }),
-    t("summit_dashboard.astro.moon.quarter", { context: "first" }),
-    t("summit_dashboard.astro.moon.gibbous", { context: "waxing" }),
-    t("summit_dashboard.astro.moon.gibbous", { context: "waxing" }),
-    t("summit_dashboard.astro.moon.gibbous", { context: "waxing" }),
-    t("summit_dashboard.astro.moon.gibbous", { context: "waxing" }),
-    t("summit_dashboard.astro.moon.full"),
-    t("summit_dashboard.astro.moon.gibbous", { context: "waning" }),
-    t("summit_dashboard.astro.moon.gibbous", { context: "waning" }),
-    t("summit_dashboard.astro.moon.gibbous", { context: "waning" }),
-    t("summit_dashboard.astro.moon.gibbous", { context: "waning" }),
-    t("summit_dashboard.astro.moon.quarter", { context: "last" }),
-    t("summit_dashboard.astro.moon.crescent", { context: "waning" }),
-    t("summit_dashboard.astro.moon.crescent", { context: "waning" }),
-    t("summit_dashboard.astro.moon.crescent", { context: "waning" }),
-    t("summit_dashboard.astro.moon.crescent", { context: "waning" }),
-  ];
-
-  const label =
-    labels[Math.min(Math.floor(phase * labels.length), labels.length - 1)];
+  const { label } =
+    phases[Math.min(Math.floor(phase * phases.length), phases.length - 1)];
 
   return (
     <WidgetBackground $variant="secondary">
-      <Styled.Label>{t("summit_dashboard.widgets.current.moon")}</Styled.Label>
+      <Styled.Label>
+        <Trans>summit_dashboard.widgets.current.moon</Trans>
+      </Styled.Label>
       <Moon phase={phase} size={70} />
-      <Styled.Unit>{label}</Styled.Unit>
+      <Styled.Unit>
+        <Trans {...label} />
+      </Styled.Unit>
     </WidgetBackground>
   );
 };
