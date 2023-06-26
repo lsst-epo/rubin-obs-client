2247490f3b896e9a9a4c92d63bd9df7eebfef2fc
Blake Mason <bmason@lsst.org>
2023-06-26 10:35:04 -0700

    [F] efd internal API enpoint + hooks fetching

--- contexts/SummitData.js
+++ contexts/SummitData.js
@@ -1,112 +1,53 @@
 import {
   createContext,
   useContext,
-  useEffect,
   useMemo,
-  useState,
-  useReducer,
 } from "react";
-import {
-  currentWeather,
-  dailyWeather,
-  hourlyWeather,
-} from "@/lib/api/queries/weather";
 import PropTypes from "prop-types";
-import queryEfd from "@/lib/api/efd";
+import useEfd from "@/lib/api/efd";
 
 export const SummitDataContext = createContext({});
 
-const loadingReducer = (state, action) => {
-  const { type, dataSet } = action;
-
-  switch (type) {
-    case "complete":
-      return { ...state, [dataSet]: false };
-    case "loading":
-      return { ...state, [dataSet]: true };
-    default:
-      throw new Error(`Unhandled action type: ${type}`);
-  }
-};
-
-const defaultLoadState = {
-  currentData: true,
-  hourlyData: true,
-  dailyData: true,
-};
-
 export const SummitDataProvider = ({ children }) => {
-  const [currentData, setCurrentData] = useState();
-  const [hourlyData, setHourlyData] = useState();
-  const [dailyData, setDailyData] = useState();
-  const [error, setError] = useState(false);
-  const [loading, dispatch] = useReducer(loadingReducer, defaultLoadState);
-
-  const fetchCurrentWeather = () =>
-    new Promise((resolve, reject) => {
-      queryEfd(currentWeather)
-        .then((value) => {
-          setCurrentData(...value);
-          resolve(...value);
-        })
-        .catch((e) => {
-          reject(e);
-        })
-        .finally(() => {
-          dispatch({ type: "complete", dataSet: "currentData" });
-        });
-    });
-
-  const fetchHourlyWeather = () =>
-    new Promise((resolve, reject) => {
-      queryEfd(hourlyWeather)
-        .then((value) => {
-          setHourlyData(value);
-          resolve(value);
-        })
-        .catch((e) => {
-          reject(e);
-        })
-        .finally(() => {
-          dispatch({ type: "complete", dataSet: "hourlyData" });
-        });
-    });
-
-  const fetchDailyWeather = () =>
-    new Promise((resolve, reject) => {
-      queryEfd(dailyWeather)
-        .then((value) => {
-          setDailyData(value);
-          resolve(value);
-        })
-        .catch((e) => {
-          reject(e);
-        })
-        .finally(() => {
-          dispatch({ type: "complete", dataSet: "dailyData" });
-        });
-    });
-
-  useEffect(() => {
-    Promise.allSettled([
-      fetchCurrentWeather(),
-      fetchHourlyWeather(),
-      fetchDailyWeather(),
-    ]).catch((e) => {
-      console.error(e);
-      setError(true);
-    });
-  }, []);
+  const {
+    data: currentData,
+    isLoading: currentIsLoading,
+    isError: currentIsError,
+  } = useEfd("current");
+  const {
+    data: hourlyData,
+    isLoading: hourlyIsLoading,
+    isError: hourlyIsError,
+  } = useEfd("hourly");
+  const {
+    data: dailyData,
+    isLoading: dailyIsLoading,
+    isError: dailyIsError,
+  } = useEfd("daily");
 
   const value = useMemo(
     () => ({
       currentData,
       hourlyData,
       dailyData,
-      loading,
-      error,
+      error: currentIsError || hourlyIsError || dailyIsError,
+      loading: {
+        currentData: currentIsLoading,
+        hourlyData: hourlyIsLoading,
+        dailyData: dailyIsLoading,
+      },
     }),
-    [currentData, hourlyData, dailyData, loading, error]
+    [
+      currentData,
+      hourlyData,
+      dailyData,
+      currentIsLoading,
+      hourlyIsLoading,
+      dailyIsLoading,
+      currentIsError,
+      hourlyIsError,
+      dailyIsError,
+    ]
   );
 
   return (
