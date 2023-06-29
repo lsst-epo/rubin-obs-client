347d63808c4b66d97547e910df5e5aa94f1b2c3f
Alexandra Goff <agoff@lsst.org>
2023-06-29 09:33:26 -0700

    [C] more responsive work

--- components/charts/ChartBase/styles.js
+++ components/charts/ChartBase/styles.js
@@ -2,5 +2,8 @@ import styled from "styled-components";
 
 export const SVG = styled.svg`
   aspect-ratio: var(--aspect-ratio);
+  min-width: var(--min-width);
+  width: 100%;
   max-height: 100%;
+  margin-inline: auto;
 `;
