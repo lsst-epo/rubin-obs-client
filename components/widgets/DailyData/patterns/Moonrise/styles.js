f4fe3d58f260c882d6e7ffee32fb1ab30f6bad71
Alexandra Goff <agoff@lsst.org>
2023-07-17 15:52:31 -0700

    [C] move timezone offset inside of lib

--- components/widgets/DailyData/patterns/Moonrise/styles.js
+++ components/widgets/DailyData/patterns/Moonrise/styles.js
@@ -2,28 +2,28 @@ import styled from "styled-components";
 
 export const Table = styled.table`
   grid-column: 1/-1;
-  border-collapse: collapse;
   font-size: 0.75rem;
+  border-collapse: collapse;
 `;
 export const Header = styled.thead``;
 export const HeaderCell = styled.th`
-  background-color: #313333;
-  border-spacing: 0;
-  border: 5px solid var(--black, #000);
-  font-weight: normal;
   height: 40px;
   padding: 0;
+  font-weight: normal;
+  border-spacing: 0;
+  background-color: #313333;
+  border: 5px solid var(--black, #000);
 
   &[scope="row"] {
     font-size: 0.75em;
   }
 `;
 export const TableCell = styled.td`
-  background-color: #313333;
-  border-spacing: 0;
-  border: 5px solid var(--black, #000);
-  font-size: 0.75em;
   height: 40px;
   padding: 0;
+  font-size: 0.75em;
   text-align: center;
+  border-spacing: 0;
+  background-color: #313333;
+  border: 5px solid var(--black, #000);
 `;
