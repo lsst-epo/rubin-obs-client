26884f7caf1da057bbb24169e5a75c93a6ceb315
Alexandra Goff <agoff@lsst.org>
2023-08-08 09:22:13 -0700

    [C] Redis setup complete

--- helpers/converters.js
+++ helpers/converters.js
@@ -2,10 +2,11 @@ import convert from "convert";
 import { defaultTempUnit, defaultWindspeedUnit } from "@/contexts/WeatherUnit";
 
 export const convertTemperature = (value, toUnit = defaultTempUnit) =>
-  convert(value, defaultTempUnit).to(toUnit);
+  convert(Number(value), defaultTempUnit).to(toUnit);
 
 export const convertWindspeed = (value, toUnit = defaultWindspeedUnit) => {
-  const adjustedValue = toUnit !== "m" ? value * 60 * 60 : value;
+  const numValue = Number(value);
+  const adjustedValue = toUnit !== "m" ? numValue * 60 * 60 : numValue;
 
   return convert(adjustedValue, defaultWindspeedUnit).to(toUnit);
 };
