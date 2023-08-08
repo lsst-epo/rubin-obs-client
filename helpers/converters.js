202ed194694c3161be8cdd191440032be98cb329
Alexandra Goff <agoff@lsst.org>
2023-08-08 16:14:59 -0700

    [C] remove Number constructors

--- helpers/converters.js
+++ helpers/converters.js
@@ -2,11 +2,10 @@ import convert from "convert";
 import { defaultTempUnit, defaultWindspeedUnit } from "@/contexts/WeatherUnit";
 
 export const convertTemperature = (value, toUnit = defaultTempUnit) =>
-  convert(Number(value), defaultTempUnit).to(toUnit);
+  convert(value, defaultTempUnit).to(toUnit);
 
 export const convertWindspeed = (value, toUnit = defaultWindspeedUnit) => {
-  const numValue = Number(value);
-  const adjustedValue = toUnit !== "m" ? numValue * 60 * 60 : numValue;
+  const adjustedValue = toUnit !== "m" ? value * 60 * 60 : value;
 
   return convert(adjustedValue, defaultWindspeedUnit).to(toUnit);
 };
