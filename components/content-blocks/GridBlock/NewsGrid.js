0258baab8d7b8ab9c78cb6228a8230a80948d513
Blake Mason <bmason@lsst.org>
2023-08-08 12:33:19 -0700

    [B] NewsGrid uses NOIRlab v2 API data format

--- components/content-blocks/GridBlock/NewsGrid.js
+++ components/content-blocks/GridBlock/NewsGrid.js
@@ -43,10 +43,10 @@ const NewsGrid = ({ items = [], limit, listTypeId, sectionHandle, pageId }) => {
                 date,
                 release_date: releaseDate,
                 description,
-                headline,
+                subtitle,
                 id,
                 image,
-                visuals,
+                images: releaseImages,
                 newsAssets,
                 postType,
                 title,
@@ -60,12 +60,12 @@ const NewsGrid = ({ items = [], limit, listTypeId, sectionHandle, pageId }) => {
                 footer={{
                   button: t("read-more"),
                 }}
-                image={image?.[0] || makeReleaseHero(visuals?.images)?.[0]}
+                image={image?.[0] || makeReleaseHero(releaseImages)?.[0]}
                 isFeature={i === 0}
                 link={uri}
                 pretitle={postType?.[0]?.title ? postType[0].title : " "}
                 subtitle={makeDateString(date || releaseDate, lang)}
-                text={makeTruncatedString(headline || description, 30)}
+                text={makeTruncatedString(subtitle || description, 30)}
                 title={title}
                 type="news"
                 showSharePopup
