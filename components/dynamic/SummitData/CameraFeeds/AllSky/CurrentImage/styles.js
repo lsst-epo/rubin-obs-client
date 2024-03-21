f2ec33d283090b2ef5bd3547990d5d638daae292
Blake Mason <bmason@lsst.org>
2024-03-21 15:02:12 -0700

    [B] CameraFeeds preview panel contains current allsky image

--- components/dynamic/SummitData/CameraFeeds/AllSky/CurrentImage/styles.js
+++ components/dynamic/SummitData/CameraFeeds/AllSky/CurrentImage/styles.js
@@ -1,8 +1,26 @@
 import styled from "styled-components";
 import { Figure as Fig } from "@rubin-epo/epo-react-lib";
+import { aHidden } from "@/styles/mixins/appearance";
 
 export const Figure = styled(Fig)`
   figcaption {
     text-align: center;
+  }
+`;
+
+export const PreviewFigure = styled(Fig)`
+  display: block;
+  width: 100%;
+  height: 100%;
+
+  img {
+    display: block;
+    width: 100%;
+    height: 100%;
+    object-fit: contain;
+  }
+
+  figcaption {
+    ${aHidden}
   }
 `;
