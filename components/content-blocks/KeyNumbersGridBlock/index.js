822c30a99dd728b063c5e082dd898a73332760a1
Alexandra Goff <agoff@lsst.org>
2024-05-24 10:37:40 -0700

    [C] key numbers markup & styling

--- components/content-blocks/KeyNumbersGridBlock/index.js
+++ components/content-blocks/KeyNumbersGridBlock/index.js
@@ -1,44 +1,73 @@
 import PropTypes from "prop-types";
-import { Container } from "@rubin-epo/epo-react-lib";
 import { mixedLinkShape } from "@/shapes/link";
 import * as Styled from "./styles";
 
+const resizeHeaderFontSize = (header) => {
+  if (header.length > 5) return "3.181rem";
+};
+
+const resizePostscriptFontSize = (postscript) => {
+  if (postscript.length > 2) return "1.636rem";
+};
+
+const resizeSubheadingFontSize = (subheading) => {
+  if (subheading?.length > 20) return "1rem";
+};
 export default function KeyNumbersGridBlock({ header, keyNumbers, mixedLink }) {
   return (
-    <Container paddingSize="medium">
+    <>
       {header && <Styled.Header>{header}</Styled.Header>}
-      <Styled.KeyNumbersGrid>
+      <Styled.KeyNumbers>
         {keyNumbers.length > 0 &&
-          [...keyNumbers].map((keyNumber) => {
-            const { id, header, subheading, footer, postscript } = keyNumber;
-
-            return (
-              <Styled.KeyNumbersGridItem key={id}>
-                <Styled.Top>
-                  <Styled.KeyNumber>
-                    <Styled.Heading>{header}</Styled.Heading>
-                    {postscript && (
-                      <Styled.Postscript>{postscript}</Styled.Postscript>
-                    )}
-                  </Styled.KeyNumber>
-                  {subheading && (
+          [...keyNumbers].map(
+            ({ id, header, subheading, footer, postscript }) => {
+              // const cleanSubheading = removeOuterParagraph(subheading);
+              return (
+                <Styled.KeyNumber key={id}>
+                  <Styled.Term>{footer}</Styled.Term>
+                  <Styled.Definition>
+                    <Styled.Number>
+                      <Styled.Heading
+                        style={{
+                          "--size-text-heading-key-numbers":
+                            resizeHeaderFontSize(header),
+                        }}
+                      >
+                        {header}
+                      </Styled.Heading>
+                      {postscript && (
+                        <Styled.Postscript
+                          style={{
+                            "--size-text-postscript-key-numbers":
+                              resizePostscriptFontSize(postscript),
+                          }}
+                        >
+                          {postscript}
+                        </Styled.Postscript>
+                      )}
+                    </Styled.Number>
                     <Styled.Subheading
-                      dangerouslySetInnerHTML={{ __html: subheading }}
+                      style={{
+                        "--size-text-subheading-key-numbers":
+                          resizeSubheadingFontSize(subheading),
+                      }}
+                      dangerouslySetInnerHTML={{
+                        __html: subheading || "&nbsp;",
+                      }}
                     />
-                  )}
-                </Styled.Top>
-                <Styled.Bottom>{footer}</Styled.Bottom>
-              </Styled.KeyNumbersGridItem>
-            );
-          })}
-      </Styled.KeyNumbersGrid>
+                  </Styled.Definition>
+                </Styled.KeyNumber>
+              );
+            }
+          )}
+      </Styled.KeyNumbers>
       {mixedLink?.url && (
         <Styled.Link
           {...mixedLink}
           className="c-buttonish c-buttonish--block"
         />
       )}
-    </Container>
+    </>
   );
 }
 
