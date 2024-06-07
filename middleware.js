feefd07311004b134edc0b93e9181a182a5354b6
Eric Rosas <ericdrosas@gmail.com>
2024-06-07 16:23:11 -0700

    Added logging to middleware for all requests Added logging to middleware for all requests

--- middleware.js
+++ middleware.js
@@ -1,21 +1,51 @@
 import { NextResponse } from "next/server";
 
+const ignorableFileExtensions = ["woff", "ico", "png", "jpeg", "jpg"];
+
 // This function can be marked `async` if using `await` inside
 export function middleware(request) {
-  const {
-    nextUrl: { search },
-  } = request;
-  const urlSearchParams = new URLSearchParams(search);
-  const params = Object.fromEntries(urlSearchParams.entries());
-  const { state: redirectPath, facebook, code } = params;
-  if (redirectPath && facebook && code) {
-    const redirectUrl = request.nextUrl.clone();
-    redirectUrl.pathname = redirectPath;
-    return NextResponse.redirect(redirectUrl);
+  // Filter out file extensions we don't care about
+  if (
+    !ignorableFileExtensions.some((e) => request.nextUrl.pathname.includes(e))
+  ) {
+    // Insight into API server:
+    const logEntry = {
+      httpRequest: {
+        name: "NEXTJS_API_SERVER_LOG",
+        hostname: process.env.HOSTNAME,
+        href: request.nextUrl.href,
+        method: request.method,
+        client_ip: request.ip,
+      },
+    };
+
+    console.info(logEntry);
+
+    const res = NextResponse.next();
+    logEntry.status = res.status;
+    logEntry.redirected = res.redirected;
+
+    return res;
+  }
+
+  // For SSO redirects:
+  if (request.nextUrl.pathname.includes("/sso-redirect/")) {
+    const {
+      nextUrl: { search },
+    } = request;
+
+    const urlSearchParams = new URLSearchParams(search);
+    const params = Object.fromEntries(urlSearchParams.entries());
+    const { state: redirectPath, facebook, code } = params;
+    if (redirectPath && facebook && code) {
+      const redirectUrl = request.nextUrl.clone();
+      redirectUrl.pathname = redirectPath;
+      return NextResponse.redirect(redirectUrl);
+    }
   }
 }
 
-// See "Matching Paths" below to learn more
+// Match anything and let the middleware decide to redirect or log
 export const config = {
-  matcher: "/sso-redirect/",
+  matcher: "/((?!_____).*)",
 };
