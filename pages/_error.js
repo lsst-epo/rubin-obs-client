54022ee258eea77a05fd23c2345589423805089b
Alexandra Goff <agoff@lsst.org>
2024-08-15 13:48:01 -0700

    chore: remove deprecated language updater

--- pages/_error.js
+++ pages/_error.js
@@ -1,7 +1,6 @@
 import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 import { useGlobalData } from "@/lib/api/global";
-import { updateI18n } from "@/lib/i18n/client";
 import { GlobalDataProvider } from "@/contexts/GlobalData";
 import ErrorPageTemplate from "@/components/templates/ErrorPage";
 import { useTranslation } from "react-i18next";
@@ -11,7 +10,6 @@ export default function Error({ statusCode }) {
   let globalData = {};
   const [lang, setLang] = useState("en");
   const [error, setError] = useState("page-not-found");
-  updateI18n(lang);
   const { t } = useTranslation();
   const errorData = {
     title: t(`${error}-title`),
