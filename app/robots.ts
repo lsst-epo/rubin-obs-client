3f36ea25e619dd283e104152a0c32a020ba1060a
Alexandra Goff <agoff@lsst.org>
2024-10-16 09:40:52 -0700

    fix: add to robots.txt (#578)
    * chore: trigger build
    
    * fix: add to robots.txt

--- app/robots.ts
+++ app/robots.ts
@@ -1,10 +1,13 @@
 import { MetadataRoute } from "next";
 
+const baseUrl = process.env.NEXT_PUBLIC_BASE_URL || "";
+
 export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: "*",
       allow: "/",
     },
+    sitemap: `${baseUrl}/sitemap_index.xml`,
   };
 }
