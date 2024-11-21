ad050ffd9033a69384c252ac1ca846cf88c6892e
Eric Rosas <ericdrosas@gmail.com>
2024-11-21 14:25:13 -0700

    Proof of concept fetching

--- app/[locale]/gallery/[gallery]/page.tsx
+++ app/[locale]/gallery/[gallery]/page.tsx
@@ -1,4 +1,5 @@
 import { FunctionComponent } from "react";
+import { getGalleryData } from "@/lib/api/galleries";
 
 // should retrieve all galleries by entry, and
 // return an array of their slugs to statically
@@ -6,13 +7,27 @@ import { FunctionComponent } from "react";
 // export async function generateStaticParams() {
 // }
 
-const Gallery: FunctionComponent<WithSearchParams<GalleryProps>> = ({
+const Gallery: FunctionComponent<WithSearchParams<GalleryProps>> = async ({
   params: { locale, gallery },
   searchParams = {},
 }) => {
+
+  const data = await getGalleryData(
+    gallery,
+    searchParams
+    // section,
+    // type,
+    // locale,
+    // previewToken
+  );
+
   return (
     <div>
       <h1>Gallery {gallery}</h1>
+      <a href="?sort=asc">Sort Ascending</a><br></br>
+      <a href="?sort=desc">Sort Descending</a>
+      <p>searchParams: {JSON.stringify(searchParams)}</p>
+      <pre>Data : {JSON.stringify(data, null, 2)} </pre>
     </div>
   );
 };
