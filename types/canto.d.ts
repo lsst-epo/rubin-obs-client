b0e1ed77f886f217f76331916db72ef5e8fd3092
Alexandra Goff <agoff@lsst.org>
2024-12-16 14:39:52 -0500

    feat: individual gallery page

--- types/canto.d.ts
+++ types/canto.d.ts
@@ -1,3 +1,9 @@
+import {
+  CantoAdditionalType,
+  CantoDamAssetInterface,
+  CantoUrlType,
+} from "@/gql/graphql";
+
 interface CantoImage {
   url: {
     directUrlPreview: string;
@@ -7,4 +13,40 @@ interface CantoImage {
   width: string;
   height: string;
   metadata: Record<string, string>;
+}
+
+interface CantoDetailedAsset {
+  additional: Pick<
+    CantoAdditionalType,
+    | "AltTextEN"
+    | "AltTextES"
+    | "CaptionEN"
+    | "CaptionES"
+    | "Credit"
+    | "TitleEN"
+    | "TitleES"
+  >;
+  approvalStatus: CantoDamAssetInterface["approvalStatus"];
+  height: CantoDamAssetInterface["height"];
+  id: CantoDamAssetInterface["id"];
+  name: CantoDamAssetInterface["name"];
+  owner: CantoDamAssetInterface["owner"];
+  ownerName: CantoDamAssetInterface["ownerName"];
+  scheme: CantoDamAssetInterface["scheme"];
+  size: CantoDamAssetInterface["size"];
+  smartTags: CantoDamAssetInterface["smartTags"];
+  tag: CantoDamAssetInterface["tag"];
+  time: CantoDamAssetInterface["time"];
+  url: Pick<
+    CantoUrlType,
+    | "directUrlOriginal"
+    | "directUrlPreview"
+    | "directUrlPreviewPlay"
+    | "download"
+    | "metadata"
+    | "preview"
+    | "HighJPG"
+    | "PNG"
+  >;
+  width: CantoDamAssetInterface["width"];
 }
