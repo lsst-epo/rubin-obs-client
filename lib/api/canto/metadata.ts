4033d5f075f4a4923254993446170fbb841a2202
Alexandra Goff <agoff@lsst.org>
2024-12-17 14:49:23 -0500

    feat: page metadata

--- lib/api/canto/metadata.ts
+++ lib/api/canto/metadata.ts
@@ -1,11 +1,85 @@
+import { Metadata } from "next";
+import { OpenGraph } from "next/dist/lib/metadata/types/opengraph-types";
 import { fallbackLng } from "@/lib/i18n/settings";
-import { CantoDetailedAsset } from "types/canto";
+import { getPreviewSize } from "./resize";
+import { CantoAssetAdditional, CantoAssetDetailed } from "../galleries/schema";
 
 export const assetCaption = (
-  asset: CantoDetailedAsset,
+  additional: CantoAssetAdditional,
   locale = fallbackLng
-) => {
+): string => {
   const localeKey = locale.toUpperCase();
+  const defaultLocaleKey = fallbackLng.toUpperCase();
 
-  return asset.additional[`Caption${localeKey}`];
+  return (
+    additional[`Caption${localeKey}`] ||
+    additional[`Caption${defaultLocaleKey}`] ||
+    ""
+  );
+};
+
+export const assetToPageMetadata = (
+  asset: CantoAssetDetailed,
+  locale: string
+): Metadata => {
+  const { name: title, additional } = asset;
+  const localeKey = locale.toUpperCase();
+  const defaultLocaleKey = fallbackLng.toUpperCase();
+
+  const description =
+    additional[`Title${localeKey}`] ||
+    additional[`Title${defaultLocaleKey}`] ||
+    undefined;
+
+  return {
+    title,
+    description,
+    openGraph: {
+      images: assetToOpenGraphImage(asset, locale),
+    },
+  };
+};
+
+export const assetToOpenGraphImage = (
+  asset: CantoAssetDetailed,
+  locale = fallbackLng
+): OpenGraph["images"] | undefined => {
+  const {
+    additional,
+    width,
+    height,
+    url: { directUrlPreview },
+  } = asset;
+
+  if (directUrlPreview) {
+    const localeKey = locale.toUpperCase();
+    const defaultLocaleKey = fallbackLng.toUpperCase();
+
+    const alt =
+      additional[`AltText${localeKey}`] ||
+      additional[`AltText${defaultLocaleKey}`] ||
+      undefined;
+    const baseAttributes = { url: directUrlPreview, alt, type: "image/jpeg" };
+
+    if (width && height) {
+      const aspectRatio = parseInt(width) / parseInt(height);
+      const previewSize = getPreviewSize(directUrlPreview);
+
+      if (aspectRatio < 1) {
+        return {
+          ...baseAttributes,
+          width: Math.round(previewSize * aspectRatio),
+          height: previewSize,
+        };
+      } else {
+        return {
+          ...baseAttributes,
+          width: previewSize,
+          height: Math.round(previewSize * aspectRatio),
+        };
+      }
+    }
+
+    return baseAttributes;
+  }
 };
