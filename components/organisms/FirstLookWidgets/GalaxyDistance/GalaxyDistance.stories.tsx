1ebea1e9e41af928f3636b33797012ec4b901acc
Alexandra Moskowitz <agoff@lsst.org>
2025-05-05 17:13:11 -0400

    feat: first look widgets

---
+++ components/organisms/FirstLookWidgets/GalaxyDistance/GalaxyDistance.stories.tsx
@@ -0,0 +1,12 @@
+import { type Meta, type StoryObj } from "@storybook/react";
+
+import GalaxyDistance from ".";
+
+const meta: Meta<typeof GalaxyDistance> = {
+  component: GalaxyDistance,
+};
+export default meta;
+
+export const Primary: StoryObj<typeof GalaxyDistance> = {
+  args: {},
+};
