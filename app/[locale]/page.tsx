4747cc5983e7a747b2205cfcdc7f0bb7c0bd76c2
Alexandra Moskowitz <agoff@lsst.org>
2025-06-02 16:34:39 -0400

    feat: structured data for Organization on homepage

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -3,19 +3,30 @@ import { notFound } from "next/navigation";
 import { getHomepage } from "@/lib/api/homepage";
 import HomePageTemplate from "@/templates/HomePage";
 import { setRequestLocale } from "next-intl/server";
+import { getOrganizationData } from "@/services/craft/globals/organization";
+import StructuredData from "@/components/atomic/StructuredData";
 
 const RootPage: FunctionComponent<LocaleProps> = async ({
   params: { locale },
 }) => {
   setRequestLocale(locale);
-  const data = await getHomepage(locale);
+
+  const [data, organization] = await Promise.all([
+    getHomepage(locale),
+    getOrganizationData(),
+  ]);
 
   // Handle 404 if there is no data
   if (!data?.id) {
     notFound();
   }
 
-  return <HomePageTemplate data={data} locale={locale} />;
+  return (
+    <>
+      <StructuredData jsonLd={organization} />
+      <HomePageTemplate data={data} locale={locale} />
+    </>
+  );
 };
 
 export default RootPage;
