d218eafc362991cfa6228104c47da152cf3d424e
Sasha Persaud <sasharavpersaud@gmail.com>
2025-06-18 10:14:29 -0700

    refactor: stubbed out typed Schedule component

---
+++ components/content-blocks/Schedule/index.tsx
@@ -0,0 +1,9 @@
+// import { FC } from "react";
+// import { FragmentType, useFragment } from "@/gql";
+// import { ScheduleBlockFragmentDoc } from "@/gql/graphql";
+
+// interface ScheduleProps extends FragmentType<typeof ScheduleBlockFragmentDoc>{}
+
+// const ScheduleBlock: FC = () => {};
+
+// export default ScheduleBlock;
