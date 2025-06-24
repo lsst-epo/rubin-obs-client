acfda3c61ebcbdc278ce21c7c4d65fba9bd5fb96
Sasha Persaud <sasharavpersaud@gmail.com>
2025-06-24 12:36:39 -0700

    refactor: added null/type checking in ScheduleBlock component

--- components/content-blocks/Schedule/index.tsx
+++ components/content-blocks/Schedule/index.tsx
@@ -1,9 +1,66 @@
-// import { FC } from "react";
-// import { FragmentType, useFragment } from "@/gql";
-// import { ScheduleBlockFragmentDoc } from "@/gql/graphql";
+import { FC } from "react";
+import { FragmentType, useFragment } from "@/gql";
+import { ScheduleBlockFragmentDoc } from "@/gql/graphql";
+import { makeDateString } from "@/helpers/dates";
+import { Container } from "@rubin-epo/epo-react-lib";
+import styled from "styled-components";
 
-// interface ScheduleProps extends FragmentType<typeof ScheduleBlockFragmentDoc>{}
+interface ScheduleProps extends FragmentType<typeof ScheduleBlockFragmentDoc> {
+  locale: string;
+}
 
-// const ScheduleBlock: FC = () => {};
+const ScheduleBlock: FC<ScheduleProps> = ({ locale, ...props }) => {
+  const { date, description, scheduleRows } = useFragment(
+    ScheduleBlockFragmentDoc,
+    props
+  );
 
-// export default ScheduleBlock;
+  const localizedDate = makeDateString(date, { locale });
+
+  return scheduleRows ? (
+    <Container>
+      <Date>{localizedDate}</Date>
+      <Description>{description}</Description>
+      <Schedule>
+        {scheduleRows?.map((row, i) => {
+          if (row?.__typename === "contentBlocks_scheduleRow_BlockType") {
+            return (
+              <div key={i} className={row.bold ? "bold" : ""}>
+                <div>
+                  {row.startTime}
+                  {row.endTime && "-" + row.endTime}
+                </div>
+                <div> {row.description}</div>
+              </div>
+            );
+          } else return null;
+        })}
+      </Schedule>
+    </Container>
+  ) : null;
+};
+
+const Date = styled.div`
+  font-weight: bold;
+`;
+
+const Description = styled.div`
+  margin: 2rem 0;
+`;
+
+const Schedule = styled.div`
+  display: grid;
+  grid-template-columns: 1fr;
+  grid-gap: 2rem;
+  margin-top: 2rem;
+  > div {
+    display: grid;
+    grid-template-columns: max-content 1fr;
+    grid-gap: 30px;
+    &.bold {
+      font-weight: bold;
+    }
+  }
+`;
+
+export default ScheduleBlock;
