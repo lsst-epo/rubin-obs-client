02cf752760cc69c7cef3b42271775aca8872cf07
Sasha Persaud <sasharavpersaud@gmail.com>
2025-06-26 09:01:39 -0700

    fix: deleted and regenerated types

--- services/craft/entries/content-blocks/schedule.ts
+++ services/craft/entries/content-blocks/schedule.ts
@@ -2,17 +2,16 @@ import { graphql } from "@/gql";
 
 const ScheduleBlockFragment = graphql(`
   fragment scheduleBlock on contentBlocks_schedule_BlockType {
-      id
-      typeHandle
-      date
-      description
-      scheduleRows: children {
-        ... on contentBlocks_scheduleRow_BlockType {
-          startTime
-          endTime
-          description
-          bold
-        }
+    id
+    typeHandle
+    date
+    description
+    scheduleRows: children {
+      ... on contentBlocks_scheduleRow_BlockType {
+        startTime
+        endTime
+        description
+        bold
       }
     }
   }
