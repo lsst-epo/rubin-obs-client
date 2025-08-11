4e407f8b5cffd51f5a7d32ee03167d85497b5d70
Eric Rosas <ericdrosas@gmail.com>
2025-08-11 15:51:27 -0700

    feat: remove contact form feat: remove contact form

--- components/organisms/Footer/index.js
+++ components/organisms/Footer/index.js
@@ -1,14 +1,14 @@
 import Social from "./Social";
 import Links from "./Links";
-import ContactForm from "./ContactForm";
+// import ContactForm from "./ContactForm";
 import { Image } from "@rubin-epo/epo-react-lib";
 import getFooter from "@/services/craft/globals/footer";
-import getContactForm from "@/services/craft/globals/contactForm";
+// import getContactForm from "@/services/craft/globals/contactForm";
 import { useTranslation } from "@/lib/i18n";
 
 export default async function Footer() {
   const footer = await getFooter();
-  const contactForm = await getContactForm();
+  // const contactForm = await getContactForm();
   const { t } = await useTranslation();
 
   if (!footer) return null;
@@ -22,17 +22,6 @@ export default async function Footer() {
       <Social />
       <Links links={links} />
       <div className="c-global-footer__main">
-        <div className="c-global-footer__form-block">
-          <h3 className="c-global-footer__heading">
-            {t("contact-form.contact-us")}
-          </h3>
-          {contactForm && (
-            <ContactForm
-              topics={contactForm.contactFormTopics}
-              className="c-global-footer__form"
-            />
-          )}
-        </div>
         <div data-cy="colophon" className="c-global-footer__colophon-block">
           {colophon && (
             <div
