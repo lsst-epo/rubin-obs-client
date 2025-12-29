5f2e9f278fa8582374262d811afd12f8ba771382
Eric Rosas <ericdrosas@gmail.com>
2025-12-29 11:52:28 -0700

    feat: added widget container for compact view feat: added widget container for compact view

--- components/dynamic/SummitData/Weather/Current/index.tsx
+++ components/dynamic/SummitData/Weather/Current/index.tsx
@@ -3,7 +3,7 @@ import { useTranslation } from "react-i18next";
 import { useWeatherUnit } from "@/contexts/WeatherUnit";
 import { useSummitData } from "@/contexts/SummitData";
 import Loader from "@/components/atomic/Loader";
-import WidgetSection from "@/components/layout/WidgetSection";
+import WidgetSection from "@/components/layout/SummitStatus/WidgetSection";
 import TemperatureCurrent from "@/components/widgets/CurrentData/patterns/Temperature";
 import WindspeedCurrent from "@/components/widgets/CurrentData/patterns/Windspeed";
 import DewpointCurrent from "@/components/widgets/CurrentData/patterns/Dewpoint";
