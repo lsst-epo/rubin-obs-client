62d2c4d933cd6c97736690e5c5a1abb722665df0
Eric Rosas <ericdrosas@gmail.com>
2026-01-13 10:03:02 -0700

    feat: added survey progress widget feat: added survey progress widget

---
+++ components/dynamic/SummitData/SurveyProgress/index.tsx
@@ -0,0 +1,51 @@
+import { useSummitData } from "@/contexts/SummitData";
+import { useTranslation } from "react-i18next";
+import Loader from "@/components/atomic/Loader";
+import WidgetSection from "@/components/layout/SummitStatus/WidgetSection";
+import styles from "./styles.module.css";
+import clsx from "clsx";
+import { ProgressRadial } from "@rubin-epo/epo-react-lib";
+
+const SurveyProgress = () => {
+  const { t } = useTranslation();
+  const {
+    summitData: { surveyProgress },
+    isLoading,
+  } = useSummitData();
+
+  const progressFormatter = (value) =>
+    Intl.NumberFormat("en-US", {
+      style: "percent",
+      maximumFractionDigits: 1,
+    }).format(value / 100);
+
+  const stillLoading = isLoading.hasura === undefined || isLoading.hasura;
+
+  return (
+    <WidgetSection
+      isCollapsible={false}
+      title={t("summit_dashboard.sections.survey_progress.title")}
+      caption={t("summit_dashboard.sections.survey_progress.caption")}
+    >
+      <div
+        className={clsx(styles.widgetBackground, styles.condensedBackground)}
+      >
+        {stillLoading ? (
+          <Loader isVisible={true} />
+        ) : (
+          <ProgressRadial
+            value={surveyProgress}
+            role={"progressbar"}
+            min={0}
+            max={100}
+            markerFormatter={progressFormatter}
+          />
+        )}
+      </div>
+    </WidgetSection>
+  );
+};
+
+SurveyProgress.displayName = "Dynamic.SurveyProgress";
+
+export default SurveyProgress;
