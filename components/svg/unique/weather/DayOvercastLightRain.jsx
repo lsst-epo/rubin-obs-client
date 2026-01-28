7185cb129939885a5283a6ea33bd70724d147fe5
Sasha Persaud <159981911+soshwosh@users.noreply.github.com>
2026-01-28 11:56:30 -0700

    feat: added weather SVGs and weather widget (#1039)
    * feat: added weather SVGs and weather widget
    
    * fix: replaced duplicate SVG

---
+++ components/svg/unique/weather/DayOvercastLightRain.jsx
@@ -0,0 +1,73 @@
+import defaultProps from "../defaultProps";
+import svgShape from "@/shapes/svg";
+
+const DayOvercastLightRain = ({ width, height }) => {
+  const uniqueProps = {
+    viewBox: "0 0 90 90",
+    width: "100%",
+    height: "auto",
+  };
+
+  const mergedSvgProps = { ...defaultProps, ...uniqueProps };
+  return (
+    <svg {...mergedSvgProps}>
+      <defs>
+        <style>
+          {`
+          .cls-1 {
+            fill: #f0f0f0;
+          }
+
+          .cls-2 {
+            fill: #fafafa;
+          }
+
+          .cls-3 {
+            fill: #f5f5f5;
+          }
+
+          .cls-4 {
+            fill: #55b9f0;
+          }
+          `}
+        </style>
+      </defs>
+      <g>
+        <path
+          className="cls-1"
+          d="M63.42,32.35h-2.18c-.83-4.22-4.91-6.96-9.13-6.14-1.15.23-2.23.71-3.17,1.41-1.93-5.69-8.11-8.74-13.8-6.81-4.22,1.43-7.13,5.28-7.37,9.73-3.39.2-5.99,3.11-5.79,6.5.19,3.3,2.96,5.86,6.26,5.8h35.18c2.9-.04,5.22-2.42,5.18-5.31-.04-2.84-2.33-5.14-5.18-5.18Z"
+        />
+        <path
+          className="cls-2"
+          d="M77.63,37.7c-.27-5.06-3.59-9.44-8.38-11.06-6.47-2.2-13.5,1.27-15.7,7.74-1.07-.8-2.3-1.35-3.61-1.61-4.79-.94-9.44,2.18-10.38,6.98h-2.47c-3.24.04-5.85,2.65-5.89,5.89-.04,3.3,2.59,6,5.89,6.04h40.01c3.76.07,6.91-2.84,7.12-6.59.22-3.86-2.72-7.17-6.58-7.39Z"
+        />
+        <path
+          className="cls-3"
+          d="M52.92,45.68h-2.47c-.94-4.79-5.59-7.92-10.38-6.98-1.31.26-2.54.81-3.61,1.61-2.2-6.47-9.23-9.94-15.7-7.74-4.79,1.63-8.11,6.01-8.38,11.06-3.86.22-6.81,3.53-6.58,7.39.22,3.76,3.36,6.67,7.12,6.59h40.01c3.3-.04,5.93-2.75,5.89-6.04-.04-3.24-2.65-5.85-5.89-5.89Z"
+        />
+      </g>
+      <g id="Group_5010" data-name="Group 5010">
+        <path
+          id="Path_1148036"
+          data-name="Path 1148036"
+          className="cls-4"
+          d="M43.05,65l-3.4-4.38-1.32,5.38c-.59,2.42.7,4.21,3.24,3.67,2.55-.54,2.99-2.7,1.47-4.67"
+        />
+      </g>
+      <g id="Group_5010-2" data-name="Group 5010-2">
+        <path
+          id="Path_1148036-2"
+          data-name="Path 1148036-2"
+          className="cls-4"
+          d="M51.05,65l-3.4-4.38-1.32,5.38c-.59,2.42.7,4.21,3.24,3.67,2.55-.54,2.99-2.7,1.47-4.67"
+        />
+      </g>
+    </svg>
+  );
+};
+
+DayOvercastLightRain.displayName = "SVG.DayOvercastLightRain";
+
+DayOvercastLightRain.propTypes = svgShape;
+
+export default DayOvercastLightRain;
--- contexts/SummitData.js
+++ contexts/SummitData.js
@@ -2,10 +2,267 @@ import { createContext, useContext, useMemo } from "react";
 import PropTypes from "prop-types";
 import { useSummitDataQuery } from "@/lib/api/summitData";
 import useAstroweather from "@/lib/api/astroweather";
+import { useTranslation } from "react-i18next";
 
 export const SummitDataContext = createContext({});
 
 export const SummitDataProvider = ({ children }) => {
+  const { t } = useTranslation();
+  const weatherSvgs = new Map();
+  weatherSvgs
+    .set(0, {
+      // Use for showing error if invalid pictocode is received
+      // TODO: Fill with Error SVG when delivered by Jose
+      daySVG: "",
+      nightSVG: "",
+      description: "Error retrieving weather",
+    })
+    .set(1, {
+      daySVG: "DayClearCloudless",
+      nightSVG: "NightClearCloudless",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.clear_cloudless_sky"
+      ),
+    })
+    .set(2, {
+      daySVG: "DayClearFewClouds",
+      nightSVG: "NightClearFewClouds",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.clear_and_few_clouds"
+      ),
+    })
+    .set(3, {
+      daySVG: "DayClearFewClouds",
+      nightSVG: "NightClearFewClouds",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.clear_and_few_clouds"
+      ),
+    })
+    .set(4, {
+      daySVG: "DayClearFewClouds",
+      nightSVG: "NightClearFewClouds",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.clear_and_few_clouds"
+      ),
+    })
+    .set(5, {
+      daySVG: "DayClearFewClouds",
+      nightSVG: "NightClearFewClouds",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.clear_and_few_clouds"
+      ),
+    })
+    .set(6, {
+      daySVG: "DayClearFewClouds",
+      nightSVG: "NightClearFewClouds",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.clear_and_few_clouds"
+      ),
+    })
+    .set(7, {
+      daySVG: "DayPartlyCloudy",
+      nightSVG: "NightPartlyCloudy",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.partly_cloudy"
+      ),
+    })
+    .set(8, {
+      daySVG: "DayPartlyCloudy",
+      nightSVG: "NightPartlyCloudy",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.partly_cloudy"
+      ),
+    })
+    .set(9, {
+      daySVG: "DayPartlyCloudy",
+      nightSVG: "NightPartlyCloudy",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.partly_cloudy"
+      ),
+    })
+    .set(10, {
+      daySVG: "DayPartlyCloudyPossibleThunder",
+      nightSVG: "NightPartlyCloudyPossibleThunder",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.mixed_with_thunderstorms"
+      ),
+    })
+    .set(11, {
+      daySVG: "DayPartlyCloudyPossibleThunder",
+      nightSVG: "NightPartlyCloudyPossibleThunder",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.mixed_with_thunderstorms"
+      ),
+    })
+    .set(12, {
+      daySVG: "DayPartlyCloudyPossibleThunder",
+      nightSVG: "NightPartlyCloudyPossibleThunder",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.mixed_with_thunderstorms"
+      ),
+    })
+    .set(13, {
+      daySVG: "DayFog",
+      nightSVG: "NightFog",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.fog"
+      ),
+    })
+    .set(14, {
+      daySVG: "DayFog",
+      nightSVG: "NightFog",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.fog"
+      ),
+    })
+    .set(15, {
+      daySVG: "DayFog",
+      nightSVG: "NightFog",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.fog"
+      ),
+    })
+    .set(16, {
+      daySVG: "DayFog",
+      nightSVG: "NightFog",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.fog"
+      ),
+    })
+    .set(17, {
+      daySVG: "DayFog",
+      nightSVG: "NightFog",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.fog"
+      ),
+    })
+    .set(18, {
+      daySVG: "DayFog",
+      nightSVG: "NightFog",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.fog"
+      ),
+    })
+    .set(19, {
+      daySVG: "DayMostlyCloudy",
+      nightSVG: "NightMostlyCloudy",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.mostly_cloudy"
+      ),
+    })
+    .set(20, {
+      daySVG: "DayMostlyCloudy",
+      nightSVG: "NightMostlyCloudy",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.mostly_cloudy"
+      ),
+    })
+    .set(21, {
+      daySVG: "DayMostlyCloudy",
+      nightSVG: "NightMostlyCloudy",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.mostly_cloudy"
+      ),
+    })
+    .set(22, {
+      daySVG: "DayOvercast",
+      nightSVG: "NightOvercast",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.overcast"
+      ),
+    })
+    .set(23, {
+      daySVG: "DayOvercastRain",
+      nightSVG: "NightOvercastRain",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.overcast_with_rain"
+      ),
+    })
+    .set(24, {
+      daySVG: "DayOvercastSnow",
+      nightSVG: "NightOvercastSnow",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.overcast_with_snow"
+      ),
+    })
+    .set(25, {
+      daySVG: "DayMostlyCloudyRain",
+      nightSVG: "NightMostlyCloudyRain",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.overcast_with_heavy_rain"
+      ),
+    })
+    .set(26, {
+      daySVG: "DayOvercastSnow",
+      nightSVG: "NightOvercastSnow",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.overcast_with_snow"
+      ),
+    })
+    .set(27, {
+      daySVG: "DayCloudyRainThunder",
+      nightSVG: "NightCloudyRainThunder",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.mostly_cloudy_with_thunderstorms"
+      ),
+    })
+    .set(28, {
+      daySVG: "DayShowersThunderstormLikely",
+      nightSVG: "NightShowersThunderstormLikely",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.showers_and_thunderstorms"
+      ),
+    })
+    .set(29, {
+      daySVG: "DayOvercastSnow",
+      nightSVG: "NightOvercastSnow",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.overcast_with_snow"
+      ),
+    })
+    .set(30, {
+      daySVG: "DayCloudyHeavyRainThunder",
+      nightSVG: "NightCloudyHeavyRainThunder",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.cloudy_heavy_rain_and_thunderstorms"
+      ),
+    })
+    .set(31, {
+      daySVG: "DayMixedShowers",
+      nightSVG: "NightMixedShowers",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.mixed_with_showers"
+      ),
+    })
+    .set(32, {
+      daySVG: "DayMostlyCloudySnow",
+      nightSVG: "NightMostlyCloudySnow",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.mostly_cloudy_with_snow"
+      ),
+    })
+    .set(33, {
+      daySVG: "DayOvercastLightRain",
+      nightSVG: "NightOvercastLightRain",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.overcast_with_light_rain"
+      ),
+    })
+    .set(34, {
+      daySVG: "DayOvercastLightSnow",
+      nightSVG: "NightOvercastLightSnow",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.overcast_with_light_snow"
+      ),
+    })
+    .set(35, {
+      daySVG: "DayMostlyCloudyMixedRainSnow",
+      nightSVG: "NightMostlyCloudyMixedRainSnow",
+      description: t(
+        "summit_dashboard.sections.weather_condition.description.overcast_with_snow_and_rain"
+      ),
+    });
+
   const {
     data: astroweatherData = {},
     isLoading: astroweatherIsLoading,
@@ -14,6 +271,7 @@ export const SummitDataProvider = ({ children }) => {
   const { data, isLoading, isError } = useSummitDataQuery();
   let summitData = {};
   let summitMedia = { items: {} };
+  let pictocode;
 
   if (data) {
     summitMedia = {
@@ -24,18 +282,23 @@ export const SummitDataProvider = ({ children }) => {
     };
 
     summitData = {
-      current: data.summitCurrentData.current,
-      daily: data.summitDailyData.daily,
-      hourly: data.summitHourlyData.hourly,
-      domeStatus: data.nightlyDigest.nightlyDigest.domeStatus,
-      exposureCount: data.nightlyDigest.nightlyDigest.exposureCount,
-      surveyProgress: data.nightlyDigest.nightlyDigest.surveyProgress,
-      alertCount: data.nightlyDigest.nightlyDigest.alertCount,
+      current: data.summitCurrentData?.current,
+      daily: data.summitDailyData?.daily,
+      hourly: data.summitHourlyData?.hourly,
+      domeStatus: data.nightlyDigest?.nightlyDigest?.domeStatus,
+      exposureCount: data.nightlyDigest?.nightlyDigest?.exposureCount,
+      surveyProgress: data.nightlyDigest?.nightlyDigest?.surveyProgress,
+      alertCount: data.nightlyDigest?.nightlyDigest?.alertCount,
     };
     if (summitData.current.dewPoint === null) {
       summitData.current.dewPoint = 1.0; // temporary code just for demoing/unblocking us
     }
+
+    const rawPictocode =
+      data.weatherData?.rawCurrentWeather?.data_current?.pictocode_detailed;
+    pictocode = rawPictocode <= 35 ? rawPictocode : 0;
   }
+
   const dateTime = Object.fromEntries(
     new Intl.DateTimeFormat("en-US", {
       timeZone: "America/Santiago",
@@ -60,6 +323,21 @@ export const SummitDataProvider = ({ children }) => {
     location: "Chile",
   };
 
+  let currentWeatherArtifacts = {};
+  if (pictocode !== undefined && pictocode <= 35) {
+    const currentWeather = weatherSvgs.get(pictocode);
+
+    // Switch to using nighttime SVG at 7pm
+    const svgName =
+      dateTime.hour < "19" ? currentWeather.daySVG : currentWeather.nightSVG;
+    const weatherDescription = currentWeather.description;
+
+    currentWeatherArtifacts = {
+      svgName,
+      weatherDescription,
+    };
+  }
+
   const value = useMemo(
     () => ({
       summitData,
@@ -75,6 +353,7 @@ export const SummitDataProvider = ({ children }) => {
         date,
         location: "Chile",
       },
+      currentWeatherArtifacts,
     }),
     [
       summitData,
@@ -85,6 +364,7 @@ export const SummitDataProvider = ({ children }) => {
       astroweatherIsLoading,
       astroweatherIsError,
       localeContextInfo,
+      currentWeatherArtifacts,
     ]
   );
 
