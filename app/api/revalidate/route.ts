f61d08ebd3e8ca4d53e5e3e8cc0370b5a9aad2fc
Jeff Innes <jeff.innes@noirlab.edu>
2026-02-18 12:05:52 -0700

    feat: update cache revalidation api endpoint to allow browser based requests from approved domains

--- app/api/revalidate/route.ts
+++ app/api/revalidate/route.ts
@@ -3,30 +3,54 @@ import { env } from "@/env";
 import revalidate from "@/services/revalidation";
 
 const REVALIDATE_SECRET_TOKEN = env.CRAFT_REVALIDATE_SECRET_TOKEN;
+// This RegEx pattern should match any authenticated URL Google generates for the on-demand revalidation tool
+// the known pattern is a GUID followed by '-apidata.googleusercontent.com'
+const GOOGLE_AUTHENTICATED_URL_PATTERN =
+  /^https:\/\/[a-zA-Z0-9]+-apidata\.googleusercontent\.com$/;
 
 export async function GET(request: NextRequest): Promise<NextResponse> {
   const uri = request.nextUrl.searchParams.get("uri");
   const secret = request.nextUrl.searchParams.get("secret");
+  const origin = request.headers.get("origin");
 
   console.info(
     `[CLIENT_REVALIDATE_STATUS] Inside of revalidate endpoint in /app/api/revalidate for: ${uri}`
   );
+
+  if (origin && !origin.match(GOOGLE_AUTHENTICATED_URL_PATTERN)) {
+    console.info(
+      `[CLIENT_REVALIDATE_STATUS] Origin Header '${origin}' does not match accepted header pattern. Request rejected.`
+    );
+    const response = NextResponse.json({
+      revalidated: false,
+      now: Date.now(),
+      message: "Bad origin",
+    });
+    return response;
+  }
+
   if (!uri) {
     console.info("[CLIENT_REVALIDATE_STATUS] No URI, returning");
-    return NextResponse.json({
+    const response = NextResponse.json({
       revalidated: false,
       now: Date.now(),
       message: "Missing path to revalidate",
     });
+
+    if (origin) response.headers.set("Access-Control-Allow-Origin", origin);
+    return response;
   }
 
   if (secret !== REVALIDATE_SECRET_TOKEN) {
     console.info("[CLIENT_REVALIDATE_STATUS] Revalidate secret is incorrect");
-    return NextResponse.json({
+    const response = NextResponse.json({
       revalidated: false,
       now: Date.now(),
       message: "Invalid token",
     });
+
+    if (origin) response.headers.set("Access-Control-Allow-Origin", origin);
+    return response;
   }
 
   if (uri) {
@@ -37,7 +61,13 @@ export async function GET(request: NextRequest): Promise<NextResponse> {
     );
     console.info(debugOutput);
 
-    return NextResponse.json({ revalidated: true, now: Date.now() });
+    const response = NextResponse.json({
+      revalidated: true,
+      now: Date.now(),
+    });
+
+    if (origin) response.headers.set("Access-Control-Allow-Origin", origin);
+    return response;
   }
 
   return NextResponse.json({
