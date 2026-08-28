3269132f90a2cc7e98240b69318b661541dc243f
Jeff Innes <jeff.innes@noirlab.edu>
2026-08-28 15:47:15 -0700

    test: update Jest config to provide accurate coverage reporting
    Additionally:
    - Set up coverage threshold boilerplate
    - Correct bugs in tooltip tests resulting from an earlier refactor of that component

--- jest.config.ts
+++ jest.config.ts
@@ -13,7 +13,43 @@ const createJestConfig = nextJest({
 const config: Config = {
   clearMocks: true,
   collectCoverage: true,
+  collectCoverageFrom: [
+    "app/**/*.{js,jsx,ts,tsx}",
+    "components/**/*.{js,jsx,ts,tsx}",
+    "contexts/**/*.{js,jsx,ts,tsx}",
+    "helpers/**/*.{js,jsx,ts,tsx}",
+    "hooks/**/*.{js,jsx,ts,tsx}",
+    "lib/**/*.{js,jsx,ts,tsx}",
+    "pages/**/*.{js,jsx,ts,tsx}",
+    "services/**/*.{js,jsx,ts,tsx}",
+    "!**/*.test.{js,jsx,ts,tsx}", // Ignore test files
+    "!**/*.stories.{js,jsx,ts,tsx}", // Ignore story files
+    "!**/mock.{js,jsx,ts,tsx}", // Ignore story files
+    "!**/*.cy.{js,jsx,ts,tsx}", // Ignore mock data files
+    "!**/styles.{js,jsx,ts,tsx}", // Ignore styled components
+    "!**/*.gen.{js,jsx,ts,tsx}", // Ignore generated files
+    "!**/*.d.ts", // Ignore type files
+    "!components/svg/**", // Ignore SVGs
+    "!lib/suncalc.js", // Ignore the 3rd party Suncalc library
+  ],
+  coverageThreshold: {
+    /**
+     * All thresholds set to zero so they are not blocking CI while we get
+     * tests implemented and determine acceptable thresholds
+     * ToDo: Incrementally set thresholds as coverage is implemented
+     */
+    global: { statements: 0, branches: 0, functions: 0, lines: 0 },
+    "./app/": { statements: 0, branches: 0, functions: 0, lines: 0 },
+    "./components/": { statements: 0, branches: 0, functions: 0, lines: 0 },
+    "./contexts/": { statements: 0, branches: 0, functions: 0, lines: 0 },
+    "./helpers/": { statements: 0, branches: 0, functions: 0, lines: 0 },
+    "./hooks/": { statements: 0, branches: 0, functions: 0, lines: 0 },
+    "./lib/": { statements: 0, branches: 0, functions: 0, lines: 0 },
+    "./pages/": { statements: 0, branches: 0, functions: 0, lines: 0 },
+    "./services/": { statements: 0, branches: 0, functions: 0, lines: 0 },
+  },
   coverageDirectory: "coverage",
+  coverageReporters: ["clover", "json", "lcov", "text", "json-summary"],
   coverageProvider: "v8",
   testEnvironment: "jsdom",
 };
